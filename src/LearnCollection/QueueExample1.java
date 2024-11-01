0c9806822fff98c657c287e424ca44cf7b59cec6
unknown <chogalesanika8@gmail.com>
2024-11-01 13:03:34 +0530

    initial

--- src/LearnCollection/QueueExample1.java
+++ src/LearnCollection/QueueExample1.java
@@ -6,7 +6,11 @@ public class QueueExample1 {
 
 	public static void main(String[] args) {
 		PriorityQueue<Integer> numbers=new PriorityQueue<Integer>();
-		
+		for(int i=1;i<=20;i++)
+		{
+			numbers.add(i);
+		}
+		System.out.println(numbers);
 	}
 	
 }
