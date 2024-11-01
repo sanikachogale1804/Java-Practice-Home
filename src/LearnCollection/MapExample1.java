0c78a98efc0df65dda9877d03d4088bdfd167ecd
unknown <chogalesanika8@gmail.com>
2024-11-01 14:38:50 +0530

    initial

--- src/LearnCollection/MapExample1.java
+++ src/LearnCollection/MapExample1.java
@@ -7,7 +7,9 @@ public class MapExample1 {
 	public static void main(String[] args) {
 		
 		HashMap<Integer, String> hashmap=new HashMap<Integer, String>();
-		
+		hashmap.put(101, "sanika");
+		hashmap.put(102, "sneha");
+		hashmap.put(103, "akshata");
 		
 	}
 
