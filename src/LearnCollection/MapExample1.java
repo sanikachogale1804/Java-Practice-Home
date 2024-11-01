39db620389a60f92aaff7078e6a35b6489b16e67
unknown <chogalesanika8@gmail.com>
2024-11-01 14:37:30 +0530

    initial

--- src/LearnCollection/MapExample1.java
+++ src/LearnCollection/MapExample1.java
@@ -1,8 +1,13 @@
 package LearnCollection;
 
+import java.util.HashMap;
+
 public class MapExample1 {
 	
 	public static void main(String[] args) {
+		
+		HashMap<Integer, String> hashmap=new HashMap<Integer, String>();
+		
 		
 	}
 
