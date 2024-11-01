a849ac7bf454d98646dcaed5b166bd572b4f8808
unknown <chogalesanika8@gmail.com>
2024-11-01 15:10:50 +0530

    initial

--- src/LearnCollection/Product.java
+++ src/LearnCollection/Product.java
@@ -34,4 +34,15 @@ public class Product {
 	{
 		this.price=price;
 	}
+	public Product()
+	{
+		
+	}
+	
+	public Product(int id,String name,int price)
+	{
+		this.id=id;
+		this.name=name;
+		this.price=price;
+	}
 }
