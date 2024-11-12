8cdff2d4976054494bfa9dd67856201a8348a03f
unknown <chogalesanika8@gmail.com>
2024-11-12 14:16:56 +0530

    initial

--- src/LearnCollection/ListExample3.java
+++ src/LearnCollection/ListExample3.java
@@ -1,8 +1,23 @@
 package LearnCollection;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class ListExample3 {
 	
 	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		for(int i=1;i<=5;i++)
+		{
+			int n=sc.nextInt();
+			list.add(n);
+		}
+		
+		Collections.sort(list);
+		System.out.println(list);
 		
 	}
 
