a10c1571eddb30c857d189a8d2900d49105ba67c
unknown <chogalesanika8@gmail.com>
2024-11-12 14:33:25 +0530

    initial

--- src/LearnCollection/ListExample4.java
+++ src/LearnCollection/ListExample4.java
@@ -17,6 +17,16 @@ public class ListExample4 {
 		Predicate<String> n=(name)->name.startsWith("a");
 		list.removeIf(n);
 		System.out.println(list);
+		
+		LinkedList<Product> products=new LinkedList<Product>();
+		products.add(new Product(101,"laptop",58000));
+		products.add(new Product(102,"mobile",68000));
+		products.add(new Product(10,"headphones",6000));
+		System.out.println(products);
+		
+		Predicate<Product> p=(product)->product.getprice()<50000;
+		products.removeIf(p);
+		System.out.println(products);
 	}
 
 }
