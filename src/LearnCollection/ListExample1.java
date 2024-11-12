09ddaad79cf7a169735cb718dc7cda3423c94ee0
unknown <chogalesanika8@gmail.com>
2024-11-12 12:06:10 +0530

    initial

--- src/LearnCollection/ListExample1.java
+++ src/LearnCollection/ListExample1.java
@@ -1,9 +1,18 @@
 package LearnCollection;
 
+import java.util.ArrayList;
+
 public class ListExample1 {
 
 	public static void main(String[] args) {
 		
+		ArrayList<Integer> List = new ArrayList<Integer>();
+		List.add(10);
+		List.add(67);
+		List.add(89);
+		List.add(45);
+		List.add(57);
+		System.out.println(List);
 	}
 	
 }
