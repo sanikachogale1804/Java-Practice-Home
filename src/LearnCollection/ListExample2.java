a0e5299b4ef2a15d4333f238272f542d95a2e4cc
unknown <chogalesanika8@gmail.com>
2024-11-12 12:22:01 +0530

    initial

--- src/LearnCollection/ListExample2.java
+++ src/LearnCollection/ListExample2.java
@@ -7,11 +7,27 @@ public class ListExample2 {
 	public static void main(String[] args) {
 		
 		ArrayList<Product> products = new ArrayList<Product>();
-		products.add(new Product(101,"Laptop",60000));
+		products.add(new Product(101,"Laptop",40000));
 		products.add(new Product(102,"mobile",68000));
 		products.add(new Product(103,"headphones",6000));
 		
 		System.out.println(products);
+		for(Product p:products)
+		{
+			if(p.getprice()<40000)
+			{
+				System.out.println(p.getprice());
+			}
+		}
+		
+		for(Product p2:products)
+		{
+			if(p2.getName().startsWith("L"))
+			{
+				System.out.println(p2.getName());
+			}
+		}
+		
 	}
 	
 }
