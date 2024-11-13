dc50a4a24ae938d42f027b15f104304d5e83100e
unknown <chogalesanika8@gmail.com>
2024-11-13 14:29:44 +0530

    initial

--- src/LearnCollection/ListExample7.java
+++ src/LearnCollection/ListExample7.java
@@ -1,8 +1,14 @@
 package LearnCollection;
 
+import java.util.Stack;
+
 public class ListExample7 {
 	
 	public static void main(String[] args) {
+		
+		Stack<Integer> stack=new Stack<Integer>();
+		stack.push(40);
+		stack.push(90);
 		
 	}
 
