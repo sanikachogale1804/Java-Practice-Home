a143628d3d92d8a3ccfc2747b3593b4fa6f8ef88
unknown <chogalesanika8@gmail.com>
2024-11-13 20:51:01 +0530

    initial

--- src/LearnCollection/ListExample8.java
+++ src/LearnCollection/ListExample8.java
@@ -1,9 +1,13 @@
 package LearnCollection;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.LinkedList;
+
 public class ListExample8 {
 	
 	public static void main(String[] args) {
-		
+		LinkedList<Integer> list=new LinkedList<Integer>();
 	}
 
 }
