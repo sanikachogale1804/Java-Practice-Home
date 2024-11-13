d247d9eee4d44e6f2e5105fbc48d45679e98fe9a
unknown <chogalesanika8@gmail.com>
2024-11-13 14:16:51 +0530

    initial

--- src/LearnCollection/ListExample6.java
+++ src/LearnCollection/ListExample6.java
@@ -1,9 +1,16 @@
 package LearnCollection;
 
+import java.util.LinkedList;
+
 public class ListExample6 {
 
 	public static void main(String[] args) {
-		
+		LinkedList<Integer> list=new LinkedList<Integer>();
+		list.add(20);
+		list.add(78);
+		list.add(10);
+		list.add(50);
+		list.add(98);
 	}
 	
 }
