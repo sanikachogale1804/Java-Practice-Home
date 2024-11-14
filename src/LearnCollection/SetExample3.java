4296840dee69ee0cb41e8336a9c119696b973fc5
unknown <chogalesanika8@gmail.com>
2024-11-14 19:55:46 +0530

    initial

--- src/LearnCollection/SetExample3.java
+++ src/LearnCollection/SetExample3.java
@@ -1,5 +1,19 @@
 package LearnCollection;
 
+import java.util.LinkedHashSet;
+
 public class SetExample3 {
+	
+	public static void main(String[] args) {
+		
+		LinkedHashSet<Integer> set=new LinkedHashSet<Integer>();
+		set.add(30);
+		set.add(2);
+		set.add(90);
+		set.add(45);
+		set.add(90);
+		System.out.println(set);
+		
+	}
 
 }
