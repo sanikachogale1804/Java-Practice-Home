0af585f8a2dca9ed1748bcea3dc58b522e0baaed
unknown <chogalesanika8@gmail.com>
2024-11-14 21:13:13 +0530

    initial

--- src/LearnCollection/SetExample4.java
+++ src/LearnCollection/SetExample4.java
@@ -1,8 +1,13 @@
 package LearnCollection;
 
+import java.util.TreeSet;
+
 public class SetExample4 {
 	
 	public static void main(String[] args) {
+	
+		TreeSet<Integer> set=new TreeSet<Integer>();
+		
 		
 	}
 
