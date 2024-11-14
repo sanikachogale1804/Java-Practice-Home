8234aac58f5b9b0801b90c6a963a10b4374e76c6
unknown <chogalesanika8@gmail.com>
2024-11-14 19:43:24 +0530

    initial

--- src/LearnCollection/SetExample2.java
+++ src/LearnCollection/SetExample2.java
@@ -1,11 +1,13 @@
 package LearnCollection;
 
 import java.util.HashSet;
+import java.util.Scanner;
 
 public class SetExample2 {
 	
 	public static void main(String[] args) {
 		
+		Scanner sc=new Scanner(System.in);
 		HashSet<String> colors=new HashSet<String>();
 		
 	}
