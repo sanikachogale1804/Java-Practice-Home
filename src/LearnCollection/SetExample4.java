a302686d958667ade7e207b8224fb2eeb284afa5
unknown <chogalesanika8@gmail.com>
2024-11-14 21:14:07 +0530

    initial

--- src/LearnCollection/SetExample4.java
+++ src/LearnCollection/SetExample4.java
@@ -7,7 +7,12 @@ public class SetExample4 {
 	public static void main(String[] args) {
 	
 		TreeSet<Integer> set=new TreeSet<Integer>();
-		
+		set.add(20);
+		set.add(87);
+		set.add(56);
+		set.add(56);
+		set.add(78);
+		set.add(null);
 		
 	}
 
