e09fbf6cb1cb357516415cf9b927c5e695e87c92
unknown <chogalesanika8@gmail.com>
2024-11-14 19:32:18 +0530

    initial

--- src/LearnCollection/SetExample1.java
+++ src/LearnCollection/SetExample1.java
@@ -1,9 +1,19 @@
 package LearnCollection;
 
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+
 public class SetExample1 {
 
 	public static void main(String[] args) {
 		
+		HashSet<Integer> hashset=new HashSet<Integer>();
+		hashset.add(30);
+		hashset.add(90);
+		hashset.add(10);
+		hashset.add(89);
+		hashset.add(900);
+		hashset.add(5);
 	}
 	
 }
