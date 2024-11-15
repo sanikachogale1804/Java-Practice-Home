ce5d237899bdf07c87ff2933b053ef75bc40992d
unknown <chogalesanika8@gmail.com>
2024-11-15 22:18:33 +0530

    initial

--- src/LearnCollection/QueueExample2.java
+++ src/LearnCollection/QueueExample2.java
@@ -1,8 +1,12 @@
 package LearnCollection;
 
+import java.util.PriorityQueue;
+
 public class QueueExample2 {
 	
 	public static void main(String[] args) {
+		PriorityQueue<Product> queue=new PriorityQueue<Product>();
+		
 		
 	}
 
