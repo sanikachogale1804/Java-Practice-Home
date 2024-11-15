fde24811990c939dd4eddefaa38b1ce70a1cb2ad
unknown <chogalesanika8@gmail.com>
2024-11-15 22:19:33 +0530

    initial

--- src/LearnCollection/QueueExample2.java
+++ src/LearnCollection/QueueExample2.java
@@ -7,6 +7,7 @@ public class QueueExample2 {
 	public static void main(String[] args) {
 		PriorityQueue<Product> queue=new PriorityQueue<Product>();
 		
+		queue.add(new Product(101,"mobile",50000));
 		
 	}
 
