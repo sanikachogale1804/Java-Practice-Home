8383cc6d52e4be41f8c3bb3d53b2144d1dfa7c9b
unknown <chogalesanika8@gmail.com>
2024-11-15 22:11:08 +0530

    initial

--- src/LearnCollection/QueueExample1.java
+++ src/LearnCollection/QueueExample1.java
@@ -6,13 +6,13 @@ public class QueueExample1 {
 
 	public static void main(String[] args) {
 		PriorityQueue<Integer> queue=new PriorityQueue<Integer>();
-		queue.add(40);
-		queue.add(20);
-		queue.add(10);
-		queue.add(2);
-		queue.add(60);
-		
-		System.out.println(queue);
+//		queue.add(40);
+//		queue.add(20);
+//		queue.add(10);
+//		queue.add(2);
+//		queue.add(60);
+//		
+//		System.out.println(queue);
 		
 	   for(int i=1;i<=20;i++)
 	   {
