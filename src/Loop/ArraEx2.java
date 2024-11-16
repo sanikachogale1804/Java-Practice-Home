156b19360fa188c7c6a8464d0f561156d183e9b2
unknown <chogalesanika8@gmail.com>
2024-11-16 14:38:31 +0530

    initial

--- src/Loop/ArraEx2.java
+++ src/Loop/ArraEx2.java
@@ -1,9 +1,23 @@
 package Loop;
 
+import java.util.Scanner;
+
 public class ArraEx2 {
 	
 	public static void main(String[] args) {
 		
+		Scanner sc=new Scanner(System.in);
+		int numbers[]=new int[5];
+		
+		for(int i=1;i<5;i++)
+		{
+			numbers[i]= sc.nextInt();
+		}
+	
+		for(int i=1;i<5;i++)
+		{
+			System.out.println(numbers[i]);
+		}
 	}
 
 }
