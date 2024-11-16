02a533fd0fb9a40a2c7a6aa38a34a7cd41c254cb
unknown <chogalesanika8@gmail.com>
2024-11-16 12:45:38 +0530

    initial

--- src/ClassesAndObjects/IfElse3.java
+++ src/ClassesAndObjects/IfElse3.java
@@ -9,5 +9,22 @@ public class IfElse3 {
 		Scanner sc=new Scanner(System.in);
 		char c=sc.next().charAt(0);
 		
+		switch(c)
+		{
+		case 'a':
+		case 'e':
+		case 'i':
+		case 'o':
+		case 'u':
+		case 'A':
+		case 'E':
+		case 'I':
+		case 'O':
+		case 'U':
+			System.out.println("Its vowel");
+		break;
+		default:
+			System.out.println("Its consonent");
 	}
+}
 }
