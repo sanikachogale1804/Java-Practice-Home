5c1db950074a0d4fd1933ae4ea914b429425ea59
unknown <chogalesanika8@gmail.com>
2024-11-16 13:09:04 +0530

    initial

--- src/Loop/FolLoopex1.java
+++ src/Loop/FolLoopex1.java
@@ -4,6 +4,13 @@ public class FolLoopex1 {
 	
 	public static void main(String[] args) {
 		
+		int n=5;
+		int fact=0;
+		for(int i=1;i<=n;i++)
+		{
+			fact=fact*i;
+		}
+		System.out.println(fact);
 	}
 
 }
