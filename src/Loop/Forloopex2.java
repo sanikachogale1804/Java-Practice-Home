3d8f6b96557ff1d3b209e97c180acc795bd707d8
unknown <chogalesanika8@gmail.com>
2024-11-16 14:20:28 +0530

    initial

--- src/Loop/Forloopex2.java
+++ src/Loop/Forloopex2.java
@@ -1,9 +1,34 @@
 package Loop;
 
+import java.util.Scanner;
+
 public class Forloopex2 {
 	
 	public static void main(String[] args) {
 		
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int count=0;
+		
+		for(int i=1;i<=n;i++)
+		{
+			if(n%i==0)
+			{
+				System.out.println(i);
+				count++;
+			}
+		}
+		System.out.println("==================");
+		System.out.println(count );
+		
+		if(count==2)
+		{
+			System.out.println("prime");
+		}
+		else
+		{
+			System.out.println("not prime");
+		}
 	}
 
 }
