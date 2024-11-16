1987242f31bf4da1c56ecbbc18e5a741f3124e64
unknown <chogalesanika8@gmail.com>
2024-11-16 12:35:48 +0530

    initial

--- src/ClassesAndObjects/IfElse2.java
+++ src/ClassesAndObjects/IfElse2.java
@@ -4,6 +4,20 @@ public class IfElse2 {
 	
 	public static void main(String[] args) {
 		
+		
+		if(true || false)
+		{
+			System.out.println("A");
+			if(true && false)
+			{
+				System.out.println("B");
+			}
+		}
+		else
+		{
+			System.out.println("C");
+		}
+		
 	}
 
 }
