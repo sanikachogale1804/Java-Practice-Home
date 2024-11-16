42c12d802bd290256b0a8af78e38570faf059c54
unknown <chogalesanika8@gmail.com>
2024-11-16 14:26:36 +0530

    initial

--- src/Loop/ForLoopex3.java
+++ src/Loop/ForLoopex3.java
@@ -1,8 +1,22 @@
 package Loop;
 
+import java.util.Scanner;
+
 public class ForLoopex3 {
 
 	public static void main(String[] args) {
+	
+		for(int n=1;n<=100;n++)
+		{
+			int count=0;
+			for(int i=1;i<=n;i++)
+			{
+				if(n%i==0)
+				{
+					count++;
+				}	
+			}	
+		}
 		
 	}
 	
