de2a727981ae6c30a3e70c738a262d1c62b4c0cd
unknown <chogalesanika8@gmail.com>
2024-11-16 14:31:18 +0530

    initial

---
+++ src/Loop/ArrayEx1.java
@@ -0,0 +1,15 @@
+package Loop;
+
+public class ArrayEx1 {
+	
+	public static void main(String[] args) {
+		int numbers[]=new int[4];
+		numbers[0]=19;
+		numbers[1]=12;
+		numbers[2]=10;
+		numbers[3]=80;
+		
+		System.out.println(numbers[0]);
+	}
+
+}
