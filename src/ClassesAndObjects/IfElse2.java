9de3af43af5d894fdf67ab859aff6d49564674e7
unknown <chogalesanika8@gmail.com>
2024-11-16 12:37:51 +0530

    initial

--- src/ClassesAndObjects/IfElse2.java
+++ src/ClassesAndObjects/IfElse2.java
@@ -1,14 +1,16 @@
 package ClassesAndObjects;
 
+import java.util.Scanner;
+
 public class IfElse2 {
 	
 	public static void main(String[] args) {
 		
-		
+	
 		if(true || false)
 		{
 			System.out.println("A");
-			if(true && false)
+			if(true && false || false)
 			{
 				System.out.println("B");
 			}
