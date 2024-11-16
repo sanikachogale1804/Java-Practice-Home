7b56e95f4c79531f007307c2c31a1df15004c453
unknown <chogalesanika8@gmail.com>
2024-11-16 14:50:37 +0530

    initial

--- src/Loop/MultiDimensionalArray.java
+++ src/Loop/MultiDimensionalArray.java
@@ -7,6 +7,17 @@ public class MultiDimensionalArray {
 		int arr1[][]= {{67,12,54},{98,89,12}};
 		System.out.println(arr1[0][2]);
 		
+		System.out.println("====================================");
+		int numbers[][]= {{67,12,54},{98,89,12}};
+		
+		for(int i=1;i<arr1.length;i++)
+		{
+			for(int j=1;j<3;j++)
+			{
+				System.out.println(numbers[i][j]);
+			}
+		}
+		
 	}
 
 }
