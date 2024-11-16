bd81ba07ee8ac58e6b3c7afb8313c8f9ce5e94dd
unknown <chogalesanika8@gmail.com>
2024-11-16 12:41:30 +0530

    initial

--- src/ClassesAndObjects/IfElse3.java
+++ src/ClassesAndObjects/IfElse3.java
@@ -1,8 +1,13 @@
 package ClassesAndObjects;
 
+import java.util.Scanner;
+
 public class IfElse3 {
 	
 	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		char c=sc.next().charAt(0);
 		
 	}
 }
