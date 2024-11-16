393a3c71de90ce7922badfdc917a439d54d65921
unknown <chogalesanika8@gmail.com>
2024-11-16 14:42:23 +0530

    initial

---
+++ src/Loop/OneDimensionalArray.java
@@ -0,0 +1,18 @@
+package Loop;
+
+public class OneDimensionalArray {
+	
+	public static void main(String[] args) {
+		
+		int numbers[]= {56,87,12,90,13};
+		System.out.println(numbers[0]);
+		System.out.println(numbers.length);
+		
+		System.out.println("======================");
+		for(int num:numbers)
+		{
+		    System.out.println(num);
+		}
+	}
+
+}
