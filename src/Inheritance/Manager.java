583187ac7da14a52a9e6e726b1171f44d06329d4
unknown <chogalesanika8@gmail.com>
2024-11-17 19:18:02 +0530

    initial

--- src/Inheritance/Manager.java
+++ src/Inheritance/Manager.java
@@ -4,5 +4,13 @@ public class Manager extends Employee{
 
 	int salary;
 	
+	Manager(String name,int age,String jobTitle,int salary)
+	{
+		this.salary=salary;
+	}
 	
+	void displayManagerInfo()
+	{
+		System.out.println("Manager Info");
+	}
 }
