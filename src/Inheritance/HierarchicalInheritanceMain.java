7205bd6c9959e03d33bb4b5c885c60702ab854b2
unknown <chogalesanika8@gmail.com>
2024-11-17 21:22:09 +0530

    initial

--- src/Inheritance/HierarchicalInheritanceMain.java
+++ src/Inheritance/HierarchicalInheritanceMain.java
@@ -4,7 +4,11 @@ public class HierarchicalInheritanceMain {
 	
 	public static void main(String[] args) {
 		
+		Circle c=new Circle("blue", 5.8);
+		System.out.println(c.calculateArea());
 		
+		Rectangle r=new Rectangle("red", 10, 5);
+		System.out.println(r.AreaOfRectangle());
 		
 	}
 }
