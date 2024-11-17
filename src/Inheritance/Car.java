a853047fa464279722fac1587e04b81a75ed64ea
unknown <chogalesanika8@gmail.com>
2024-11-17 18:51:51 +0530

    initial

--- src/Inheritance/Car.java
+++ src/Inheritance/Car.java
@@ -4,6 +4,13 @@ public class Car extends Vehicle{
 
 	String FuelType;
 	
+	Car(String make,String model,String FuelType)
+	{
+		this.make=make;
+		this.model=model;
+		this.FuelType=FuelType;
+	}
+	
 	public void startEngine()
 	{
 		System.out.println("starting the vehicles engine");
--- src/Inheritance/SingleInheritanceMain.java
+++ src/Inheritance/SingleInheritanceMain.java
@@ -4,9 +4,11 @@ public class SingleInheritanceMain {
 	
 	public static void main(String[] args) {
 		
-		Car c = new Car();
-		
-		
+		Car c=new Car("Car", "Porsche", "petrol");
+		System.out.println(c.make);
+		System.out.println(c.model);
+		System.out.println(c.FuelType);
+		c.startEngine();
 	}
 
 }
