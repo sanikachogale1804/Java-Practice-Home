0753564fabc04c78d1ba158ba67dff2f644f2e5f
unknown <chogalesanika8@gmail.com>
2024-11-17 18:25:31 +0530

    initial

--- src/Practice/staticExample1.java
+++ src/Practice/staticExample1.java
@@ -15,13 +15,13 @@ public class staticExample1 {
 	
 	public static void main(String[] args) {
 		
-		Counter counter = new Counter();
-		new Counter();
-		new Counter();
+//		Counter counter = new Counter();
+//		new Counter();
+//		new Counter();
 		
 		for(int i=1;i<=100;i++)
 		{
-			
+			new Counter();
 		}
 	}
 
