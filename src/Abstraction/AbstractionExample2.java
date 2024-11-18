d97993a7d5654f3510ee1e90a395e31835a1e6e0
unknown <chogalesanika8@gmail.com>
2024-11-18 18:34:51 +0530

    initial

--- src/Abstraction/AbstractionExample2.java
+++ src/Abstraction/AbstractionExample2.java
@@ -5,6 +5,14 @@ abstract class Animal
 	abstract void sound();
 }
 
+class Dog extends Animal
+{
+	void sound()
+	{
+		System.out.println("bho-bho");
+	}
+}
+
 public class AbstractionExample2 {
 	
 	public static void main(String[] args) {
