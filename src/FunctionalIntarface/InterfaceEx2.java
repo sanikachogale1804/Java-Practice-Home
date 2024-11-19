d3dd1b73fe8c07cfe3cbf356835d8ec85d930e44
unknown <chogalesanika8@gmail.com>
2024-11-19 19:42:54 +0530

    initial

--- src/FunctionalIntarface/InterfaceEx2.java
+++ src/FunctionalIntarface/InterfaceEx2.java
@@ -1,5 +1,11 @@
 package FunctionalIntarface;
 
+@FunctionalInterface
+interface NumberFilter
+{
+	boolean test(int number);
+}
+
 public class InterfaceEx2 {
 	
 	public static void main(String[] args) {
