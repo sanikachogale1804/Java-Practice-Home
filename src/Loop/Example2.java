e3ab88e469b735b6fba1b6b01df2ba4d3c6d3a2a
unknown <chogalesanika8@gmail.com>
2024-11-20 22:23:49 +0530

    initial

--- src/Loop/Example2.java
+++ src/Loop/Example2.java
@@ -7,6 +7,7 @@ public class Example2 {
 	public static void main(String[] args) {
 		
 		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the digits:");
 		int num = sc.nextInt();
 	}
 
