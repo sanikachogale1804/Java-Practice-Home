6b4d1cafaf757b986232daa1825441e169b1a898
unknown <chogalesanika8@gmail.com>
2024-11-21 19:26:14 +0530

    initial

--- src/StringExample/StringExample4.java
+++ src/StringExample/StringExample4.java
@@ -11,7 +11,15 @@ public class StringExample4 {
 			System.out.println(s1.charAt(i));
 		}
 		
+		String str="I love Java Programming";
 		
+		StringBuilder reversedstr=new StringBuilder();
+		
+		for(int i=(str.length()-1);i>=0;i--)
+		{
+			reversedstr.append(i);
+		}
+	System.out.println(reversedstr.toString());	
 	}
 
 }
