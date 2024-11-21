fe328464feda4fb35cdc6653f6c08c9bc868d8ee
unknown <chogalesanika8@gmail.com>
2024-11-21 18:25:38 +0530

    initial

--- src/StringExample/StringExample2.java
+++ src/StringExample/StringExample2.java
@@ -4,6 +4,13 @@ public class StringExample2 {
 	
 	public static void main(String[] args) {
 		
+		String a="Hello";
+		String b="Hello";
+		
+		String c=new String("hello world");
+		String d=new String("hello world");
+		
+		
 	}
 
 }
