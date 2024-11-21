4f412545e0b4e912be479540504d9485134de35e
unknown <chogalesanika8@gmail.com>
2024-11-21 18:26:59 +0530

    initial

--- src/StringExample/StringExample2.java
+++ src/StringExample/StringExample2.java
@@ -10,7 +10,10 @@ public class StringExample2 {
 		String c=new String("hello world");
 		String d=new String("hello world");
 		
+		System.out.println("============================");
 		
+		System.out.println(a==b);
+		System.out.println(c==d);
 	}
 
 }
