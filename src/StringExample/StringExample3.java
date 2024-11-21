3e069f06e5d951aa7d317676e75e82a125c654f9
unknown <chogalesanika8@gmail.com>
2024-11-21 18:46:36 +0530

    initial

--- src/StringExample/StringExample3.java
+++ src/StringExample/StringExample3.java
@@ -4,6 +4,11 @@ public class StringExample3 {
 	
 	public static void main(String[] args) {
 		
+		System.out.println("   h      e    ll     o        ".trim()+"Wolrd");
+		
+		System.out.println("sanika".length());
+		System.out.println("aple".charAt(2));
+		System.out.println("Sneha".charAt(4));
 	}
 
 }
