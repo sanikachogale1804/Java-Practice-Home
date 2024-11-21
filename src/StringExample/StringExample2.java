ecc39f3c14f30df79d46a75c0b379022b00e420f
unknown <chogalesanika8@gmail.com>
2024-11-21 18:28:34 +0530

    initial

--- src/StringExample/StringExample2.java
+++ src/StringExample/StringExample2.java
@@ -5,15 +5,14 @@ public class StringExample2 {
 	public static void main(String[] args) {
 		
 		String a="Hello";
-		String b="Hello";
+		String b="HELLO";
 		
 		String c=new String("hello world");
 		String d=new String("hello world");
 		
 		System.out.println("============================");
+	
 		
-		System.out.println(a==b);
-		System.out.println(c==d);
 	}
 
 }
