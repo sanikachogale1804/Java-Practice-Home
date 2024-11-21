cec2268c9566ced53e4d52482e9332146fec7232
unknown <chogalesanika8@gmail.com>
2024-11-21 19:30:25 +0530

    initial

--- src/StringExample/StringExample4.java
+++ src/StringExample/StringExample4.java
@@ -12,12 +12,17 @@ public class StringExample4 {
 		}
 		
 		String str="I love Java Programming";
+		String[] word=str.split(" ");
 		
 		StringBuilder reversedstr=new StringBuilder();
 		
-		for(int i=(str.length()-1);i>=0;i--)
+		for(int i=word.length-1;i>=0;i--)
 		{
-			reversedstr.append(i);
+			reversedstr.append(word[i]);
+			if(i>0)
+			{
+				reversedstr.append(" ");
+			}
 		}
 	System.out.println(reversedstr.toString());	
 	}
