b72d94c638e61be5fef62517409b37e67c3c6223
unknown <chogalesanika8@gmail.com>
2024-11-22 19:26:23 +0530

    initial

--- src/StreamPractice/Example3.java
+++ src/StreamPractice/Example3.java
@@ -1,10 +1,16 @@
 package StreamPractice;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Example3 {
 	
 	public static void main(String[] args) {
 		
+		List<Integer> list = Arrays.asList(34,89,21,79,25,17);
 		
+		System.out.println(list);
+		//list.stream().filter((n)->n>20).collect(null)
 		
 	}
 
