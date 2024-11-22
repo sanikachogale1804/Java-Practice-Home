d98286816bcf5ff049af8f3d3872306f4d8334e0
unknown <chogalesanika8@gmail.com>
2024-11-22 20:11:47 +0530

    initial

--- src/StreamPractice/Example6.java
+++ src/StreamPractice/Example6.java
@@ -1,8 +1,19 @@
 package StreamPractice;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 public class Example6 {
 	
 	public static void main(String[] args) {
+		
+		List<Integer> list = Arrays.asList(12,89,67,35,82,39,56);
+		Optional<Integer> reduce = list.stream().reduce((a,b)->a+b);
+		System.out.println(reduce);
+		
+		Integer integer = reduce.get();
+		System.out.println(integer);
 		
 	}
 
