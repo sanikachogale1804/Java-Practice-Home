f93f8f5b398db237ccd9252abb9263c2afd1f49c
unknown <chogalesanika8@gmail.com>
2024-11-22 19:52:29 +0530

    initial

--- src/StreamPractice/Example5.java
+++ src/StreamPractice/Example5.java
@@ -1,8 +1,19 @@
 package StreamPractice;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class Example5 {
 	
 	public static void main(String[] args) {
+		
+		List<Integer> numbers = Arrays.asList(12,79,67,34,90,13);
+	    Function<Integer, Integer> squareFunction=(n)->n*n;
+		List<Integer> square = numbers.stream().map(squareFunction).collect(Collectors.toList());
+		System.out.println(square);
+		
 		
 	}
 
