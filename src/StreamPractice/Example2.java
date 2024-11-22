a756b8e7dcb478a201b23e65af0ca298efefae89
unknown <chogalesanika8@gmail.com>
2024-11-22 19:13:48 +0530

    initial

--- src/StreamPractice/Example2.java
+++ src/StreamPractice/Example2.java
@@ -1,8 +1,20 @@
 package StreamPractice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Example2 {
 	
 	public static void main(String[] args) {
+		
+		List<Product> list=new ArrayList<Product>();
+		
+		list.add(new Product(101,"laptop",60000));
+		list.add(new Product(102,"mobile",40000));
+		list.add(new Product(103,"Tv",90000));
+		list.add(new Product(104,"washingmachine",70000));
+		
+		System.out.println(list);
 		
 	}
 
