5af7d10d119e0e5611cab474eaf8a5133abf0114
unknown <chogalesanika8@gmail.com>
2024-11-22 18:57:35 +0530

    initial

--- src/StreamPractice/Example1.java
+++ src/StreamPractice/Example1.java
@@ -20,9 +20,10 @@ public class Example1 {
 		list.stream().forEach((i)->System.out.println(i));
 		
 		System.out.println("=================================");
-		List<String> list2 = Arrays.asList("sanika","riya","sneha","tushar","akshata");
+		List<String> list2 = Arrays.asList("sanika","riya","sneha","tushar","akshata","siddhesh");
 		Stream<String> nameConsumer = list2.stream();
-		
+		Consumer<String> name=(n)->System.out.println(n.charAt((n.length()-1)));
+		nameConsumer.forEach(name);
 		
 	}
 
