660356924b2edddba1cba2f945fe890b7b8f7969
unknown <chogalesanika8@gmail.com>
2024-11-22 18:43:10 +0530

    initial

--- src/StreamPractice/Example1.java
+++ src/StreamPractice/Example1.java
@@ -7,7 +7,10 @@ public class Example1 {
 	public static void main(String[] args) {
 		
 		ArrayList list = new ArrayList<Integer>();
-		
+		for(int i=1;i<=20;i++)
+		{
+			 list.add(i);
+		}
 	}
 
 }
