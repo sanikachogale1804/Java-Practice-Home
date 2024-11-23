3e49a06b139c93a52e9ae8082905d7e1b35398c7
unknown <chogalesanika8@gmail.com>
2024-11-23 18:45:05 +0530

    initial

--- src/StreamApi/Example8.java
+++ src/StreamApi/Example8.java
@@ -8,12 +8,14 @@ public class Example8 {
 	
 	public static void main(String[] args) {
 		
-		List<Integer> numbers = Arrays.asList(12,79,67,45,98,34);
+//		List<Integer> numbers = Arrays.asList(12,79,67,45,98,34);
+		List<Integer> numbers = Arrays.asList();
 		Optional<Integer> first = numbers.stream().findFirst();
 		System.out.println(first);
+//		
+//		Integer integer = first.get();
+//		System.out.println(integer);
 		
-		Integer integer = first.get();
-		System.out.println(integer);
 		
 		
 	}
