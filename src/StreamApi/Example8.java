3849f7d2ec84cab0b2826060b45bf959918b8f6d
unknown <chogalesanika8@gmail.com>
2024-11-23 18:41:55 +0530

    initial

--- src/StreamApi/Example8.java
+++ src/StreamApi/Example8.java
@@ -1,8 +1,16 @@
 package StreamApi;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 public class Example8 {
 	
 	public static void main(String[] args) {
+		
+		List<Integer> numbers = Arrays.asList(12,79,67,45,98,34);
+		Optional<Integer> first = numbers.stream().findFirst();
+		
 		
 	}
 
