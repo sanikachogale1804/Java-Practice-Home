37a940fde5b1e319334f8d0eda06aebea97f78f3
unknown <chogalesanika8@gmail.com>
2024-11-23 18:24:05 +0530

    initial

--- src/StreamPractice/Example7.java
+++ src/StreamPractice/Example7.java
@@ -1,8 +1,12 @@
 package StreamPractice;
 
+import java.util.ArrayList;
+
 public class Example7 {
 	
 	public static void main(String[] args) {
+		
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
 		
 	}
 
