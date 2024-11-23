71032fd3f514b0021f8c54c75d1e5bd58946c381
unknown <chogalesanika8@gmail.com>
2024-11-23 18:10:17 +0530

    initial

--- src/StreamPractice/Example6.java
+++ src/StreamPractice/Example6.java
@@ -11,13 +11,8 @@ public class Example6 {
 
 	public static <E> void main(String[] args) {
 		
-		List<Integer> list = Arrays.asList(12,89,67,35,82,39,56);
-		Optional<Integer> reduce = list.stream().reduce((a,b)->a+b);
-		System.out.println(reduce);
-	    
-		Integer integer2 = reduce.get();
-System.out.println(integer2);
+     ArrayList<E> arrayList = new ArrayList<E>();
+     
 		
-
 }
 }
