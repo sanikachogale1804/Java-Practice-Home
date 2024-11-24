5b601aa6ad7007e3256489d8b61389dc6ca2ef5b
unknown <chogalesanika8@gmail.com>
2024-11-24 19:48:47 +0530

    initial

--- src/StreamPractice/Example9.java
+++ src/StreamPractice/Example9.java
@@ -2,13 +2,14 @@ package StreamPractice;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Example9 {
 	
 	public static void main(String[] args) {
 		List<Integer> numbers=Arrays.asList(12,79,56,90,63,39);
 		
-		
+		numbers.stream().filter((n)->n>20);
 		
 	}
 
