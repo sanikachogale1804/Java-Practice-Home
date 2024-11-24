f394dc922a53aaaf7227c2b669c0107c40512d39
unknown <chogalesanika8@gmail.com>
2024-11-24 19:49:57 +0530

    initial

--- src/StreamPractice/Example9.java
+++ src/StreamPractice/Example9.java
@@ -9,7 +9,7 @@ public class Example9 {
 	public static void main(String[] args) {
 		List<Integer> numbers=Arrays.asList(12,79,56,90,63,39);
 		
-		numbers.stream().filter((n)->n>20);
+		numbers.stream().filter((n)->n%2==0).forEach((n)->System.out.println(n));
 		
 	}
 
