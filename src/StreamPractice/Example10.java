2fbf172468398190717c37fc690153523a8b4c68
unknown <chogalesanika8@gmail.com>
2024-11-24 20:19:41 +0530

    initial

--- src/StreamPractice/Example10.java
+++ src/StreamPractice/Example10.java
@@ -2,13 +2,16 @@ package StreamPractice;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Example10 {
 	
 	public static void main(String[] args) {
 		List<Integer> numbers = Arrays.asList(12,79,67,34,90,13);
 		
-		
+		List<Integer> multipyby10 = numbers.stream().map((n)->n*10).collect(Collectors.toList());
+		System.out.println(multipyby10);
 		
 	}
 
