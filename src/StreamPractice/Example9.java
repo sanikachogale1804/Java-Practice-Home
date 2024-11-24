9ed78a6ae7f507c0a0aad6c90e5b5d8a1a393667
unknown <chogalesanika8@gmail.com>
2024-11-24 19:57:34 +0530

    initial

--- src/StreamPractice/Example9.java
+++ src/StreamPractice/Example9.java
@@ -8,10 +8,14 @@ import java.util.stream.Collectors;
 public class Example9 {
 	
 	public static void main(String[] args) {
-		  List<String> words = Arrays.asList("apple", "banana", "cherry", "date", "kiwi", "pineapple");
+		List<User> people = Arrays.asList(
+	            new User("Alice", 30),
+	            new User("Bob", 17),
+	            new User("Charlie", 20),
+	            new User("David", 16)
+	        );
 		  
-		  words.stream().filter((word)->word.length()>5).forEach((word)->System.out.println(word));
-		  
+		people.stream().filter((name)->name.getAge()>20).forEach((name)->System.out.println(name.getName()));
 	}
 
 }
