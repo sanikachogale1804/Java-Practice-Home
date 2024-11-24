e635db4fab3a51ca1923fe4549c9784686d09480
unknown <chogalesanika8@gmail.com>
2024-11-24 19:54:55 +0530

    initial

--- src/StreamPractice/Example9.java
+++ src/StreamPractice/Example9.java
@@ -8,11 +8,10 @@ import java.util.stream.Collectors;
 public class Example9 {
 	
 	public static void main(String[] args) {
-		List<Integer> numbers=Arrays.asList(12,79,56,90,63,39);
-		
-		numbers.stream().filter((n)->n%2==0).forEach((n)->System.out.println(n));
-		List<Integer> divedeby2 = numbers.stream().filter((n)->n%2==0).collect(Collectors.toList());
-		System.out.println(divedeby2);
+		  List<String> words = Arrays.asList("apple", "banana", "cherry", "date", "kiwi", "pineapple");
+		  
+		  words.stream().filter((word)->word.length()>5).forEach((word)->System.out.println(word));
+		  
 	}
 
 }
