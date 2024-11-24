c7ad2812d33c5226fc7c4c6571f6c2f71672193f
unknown <chogalesanika8@gmail.com>
2024-11-24 20:24:16 +0530

    initial

--- src/StreamPractice/Example10.java
+++ src/StreamPractice/Example10.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Example10 {
 	
@@ -15,7 +16,8 @@ public class Example10 {
 		            new Employee("David", 4000)
 		        );
 		 
-		 
+		 Stream<String> map = employees.stream().map((name)->name.getName()+name.getSalary()*10);
+		 System.out.println(map);
 		 
 	}
 
