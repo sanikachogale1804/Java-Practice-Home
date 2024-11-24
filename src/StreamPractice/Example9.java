ad47a252bec025c3eb3597fdb1b658973f11f1cd
unknown <chogalesanika8@gmail.com>
2024-11-24 19:46:12 +0530

    initial

---
+++ src/StreamPractice/Example9.java
@@ -0,0 +1,15 @@
+package StreamPractice;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Example9 {
+	
+	public static void main(String[] args) {
+		List<Integer> numbers=Arrays.asList(12,79,56,90,63,39);
+		
+		
+		
+	}
+
+}
