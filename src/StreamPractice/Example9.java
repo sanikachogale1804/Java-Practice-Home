d337c430af2853bda8eb3f52bbff5f097da750de
unknown <chogalesanika8@gmail.com>
2024-11-24 19:52:16 +0530

    initial

--- src/StreamPractice/Example9.java
+++ src/StreamPractice/Example9.java
@@ -3,6 +3,7 @@ package StreamPractice;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Example9 {
 	
@@ -10,7 +11,8 @@ public class Example9 {
 		List<Integer> numbers=Arrays.asList(12,79,56,90,63,39);
 		
 		numbers.stream().filter((n)->n%2==0).forEach((n)->System.out.println(n));
-		
+		List<Integer> divedeby2 = numbers.stream().filter((n)->n%2==0).collect(Collectors.toList());
+		System.out.println(divedeby2);
 	}
 
 }
