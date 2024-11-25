d592933c726f9780528ae590194927004effe3f7
unknown <chogalesanika8@gmail.com>
2024-11-25 18:51:58 +0530

    initial

--- src/MultiThreading/Example3.java
+++ src/MultiThreading/Example3.java
@@ -12,7 +12,13 @@ class T5 extends Thread
 }
 public class Example3 {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+		
+		T5 t5=new T5();
+		t5.setDaemon(true);
+		t5.start();
+		
+		Thread.sleep(1000);
 		
 	}
 
