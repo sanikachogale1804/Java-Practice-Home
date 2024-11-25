40f649ec83d42709df68c08ddf0f9c0990f685df
unknown <chogalesanika8@gmail.com>
2024-11-25 18:23:18 +0530

    initial

---
+++ src/MultiThreading/Example1.java
@@ -0,0 +1,9 @@
+package MultiThreading;
+
+public class Example1 {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
--- src/StreamPractice/Example9.java
+++ src/StreamPractice/Example9.java
@@ -16,7 +16,7 @@ public class Example9 {
 	        );
 		  
 		people.stream().filter((name)->name.getAge()>20).forEach((name)->System.out.println(name.getName()));
-		people.stream().filter(null)
+		
 	}
 
 }
