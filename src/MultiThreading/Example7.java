62bfb4959134d07d5091e38cadc05ec02eceab4d
unknown <chogalesanika8@gmail.com>
2024-11-25 19:35:27 +0530

    initial

--- src/MultiThreading/Example7.java
+++ src/MultiThreading/Example7.java
@@ -4,7 +4,16 @@ public class Example7 {
 	
 	public static void main(String[] args) {
 		
+		Counter counter=new Counter();
+			Thread t1=new Thread(()->{
+				for(int i=1;i<=50000;i++)
+				{
+					counter.increment();
+				}
+			});
+		t1.start();
 		
+		System.out.println(counter.count);
 		
 	}
 
