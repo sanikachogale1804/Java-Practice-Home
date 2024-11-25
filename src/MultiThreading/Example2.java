f948bb21853e7630b4a6656788aea4a52443f634
unknown <chogalesanika8@gmail.com>
2024-11-25 18:39:44 +0530

    initial

---
+++ src/MultiThreading/Example2.java
@@ -0,0 +1,22 @@
+package MultiThreading;
+
+class T3 extends Thread
+{
+	public void run() {
+	for(int i=1;i<=10;i++)
+	{
+		System.out.println("*");
+	}
+	}
+}
+
+
+public class Example2 {
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
