267934eb4541e95d157564e102af423769aebf95
unknown <chogalesanika8@gmail.com>
2024-11-25 18:55:43 +0530

    initial

--- src/MultiThreading/Example4.java
+++ src/MultiThreading/Example4.java
@@ -1,8 +1,25 @@
 package MultiThreading;
 
+class T6 extends Thread
+{
+	public void run()
+	{
+		while(true)
+		{
+			System.out.println("hello");
+		}
+	}
+}
+
 public class Example4 {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+		
+		T6 t6=new T6();
+		t6.setDaemon(true);
+		t6.start();
+		
+		Thread.sleep(500);
 		
 	}
 
