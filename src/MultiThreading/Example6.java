5e6efcd215b28db94a35516d6921e10f91adf037
unknown <chogalesanika8@gmail.com>
2024-11-25 19:14:06 +0530

    initial

--- src/MultiThreading/Example6.java
+++ src/MultiThreading/Example6.java
@@ -4,6 +4,11 @@ public class Example6 {
 
 	public static void main(String[] args) {
 		
+		Runnable r1=()->System.out.println("Lambda with multithreading");
+		Thread t1=new Thread(r1);
+		t1.start();
+		
+		
 	}
 	
 }
