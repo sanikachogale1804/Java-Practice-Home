221c4531a62b324b772edee764e0fe43e596a0f7
unknown <chogalesanika8@gmail.com>
2024-11-25 19:17:36 +0530

    initial

--- src/MultiThreading/Example6.java
+++ src/MultiThreading/Example6.java
@@ -8,6 +8,16 @@ public class Example6 {
 		Thread t1=new Thread(r1);
 		t1.start();
 		
+		Thread t2=new Thread(()->System.out.println("lambda expression"));
+		t2.start();
+		
+		Thread t3=new Thread(()->{
+			for(int i=1;i<=10;i++)
+			{
+				System.out.println("sanika");
+			}
+		});
+		t3.start();
 		
 	}
 	
