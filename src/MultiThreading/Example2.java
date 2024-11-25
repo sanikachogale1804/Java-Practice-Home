48bfc131c2b7d18e56430f9b1c663c0eb30ecf7c
unknown <chogalesanika8@gmail.com>
2024-11-25 18:44:08 +0530

    initial

--- src/MultiThreading/Example2.java
+++ src/MultiThreading/Example2.java
@@ -6,6 +6,12 @@ class T3 extends Thread
 	for(int i=1;i<=10;i++)
 	{
 		System.out.println("*");
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	}
 }
@@ -17,6 +23,12 @@ class T4 extends Thread
 		for(int i=1;i<=10;i++)
 		{
 			System.out.println("$");
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 }
