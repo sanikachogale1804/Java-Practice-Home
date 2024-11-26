780a454e3ee97f79c491503cdaaa4378d50d78ab
unknown <chogalesanika8@gmail.com>
2024-11-26 19:00:12 +0530

    initial

--- src/Operators/TernaryOperator.java
+++ src/Operators/TernaryOperator.java
@@ -6,6 +6,13 @@ public class TernaryOperator {
 		
 		System.out.println(false?"sanika":"sneha");
 		
+		int a=50;
+		int b=30;
+		
+		int biggervalue=a>b?a:b;
+		System.out.println(biggervalue);
+		System.out.println(a>b?"a is greater":"b is greater");
+		
 	}
 
 }
