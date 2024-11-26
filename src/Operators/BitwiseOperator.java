71bdaf99f4f64a783e293f4af6e33475693e3e84
unknown <chogalesanika8@gmail.com>
2024-11-26 18:54:04 +0530

    initial

--- src/Operators/BitwiseOperator.java
+++ src/Operators/BitwiseOperator.java
@@ -4,6 +4,16 @@ public class BitwiseOperator {
 	
 	public static void main(String[] args) {
 		
+		System.out.println(15&18);  //  1111
+                                     //10010
+                                    //--------------
+		                            // 00010 
+		
+		System.out.println(20|40);  //010100
+                                    //101000
+                                    //------------
+                                    //111100
+		
 	}
 
 }
