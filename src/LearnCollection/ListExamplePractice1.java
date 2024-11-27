5619596a893652de9fd923e895b3cd4b04f08b03
unknown <chogalesanika8@gmail.com>
2024-11-27 12:07:37 +0530

    initial

--- src/LearnCollection/ListExamplePractice1.java
+++ src/LearnCollection/ListExamplePractice1.java
@@ -1,8 +1,13 @@
 package LearnCollection;
 
+import java.util.ArrayList;
+
 public class ListExamplePractice1 {
 	
 	public static void main(String[] args) {
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("apple");
+		
 		
 	}
 
