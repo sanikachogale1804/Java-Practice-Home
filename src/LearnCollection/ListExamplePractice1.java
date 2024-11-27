dca770e58b1cc2a989d4c213f14e11d25b2a4041
unknown <chogalesanika8@gmail.com>
2024-11-27 12:43:03 +0530

    initial

--- src/LearnCollection/ListExamplePractice1.java
+++ src/LearnCollection/ListExamplePractice1.java
@@ -2,6 +2,7 @@ package LearnCollection;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.function.Predicate;
 
 public class ListExamplePractice1 {
 	
@@ -10,14 +11,24 @@ public class ListExamplePractice1 {
 		list.add(56);
 		list.add(60);
 		list.add(45);
-		list.add(null);
         System.out.println(list);
 		System.out.println("===================");
 		
-		list.add(0,34);
+//		list.add(0,34);
+//		System.out.println(list);
+//		
+//		System.out.println(list.get(2));
+//		System.out.println("===================");
+//		Integer i = list.set(3, 30);
+//		System.out.println(i);
+//		System.out.println(list);
+		
+//		Predicate<Integer> even=(num)->num%2==0;
+//		list.removeIf(even);
+		
+		list.removeIf((num)->num%2==0);
 		System.out.println(list);
 		
-		System.out.println(list.get(2));
 	}
 
 }
