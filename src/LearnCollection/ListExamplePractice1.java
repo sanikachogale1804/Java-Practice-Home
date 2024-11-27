9985b406d89f3d1b99c69d9b099262a70e746c29
unknown <chogalesanika8@gmail.com>
2024-11-27 13:06:58 +0530

    initial

--- src/LearnCollection/ListExamplePractice1.java
+++ src/LearnCollection/ListExamplePractice1.java
@@ -7,12 +7,21 @@ import java.util.Comparator;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 import java.util.function.Predicate;
 
 public class ListExamplePractice1 {
 	
 	public static void main(String[] args) {
-		
+		Stack<Integer> stack=new Stack<Integer>();
+		//System.out.println(stack.empty());
+		stack.push(20);
+		stack.push(45);
+		stack.push(67);
+		stack.push(67);
+		stack.push(56);
+		System.out.println(stack);
+		System.out.println("=====================");
 		
 		
 	}
