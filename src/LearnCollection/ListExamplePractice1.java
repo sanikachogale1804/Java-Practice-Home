1efc2544be4466c0b2ec2182eea2347e551eb26d
unknown <chogalesanika8@gmail.com>
2024-11-27 12:33:23 +0530

    initial

--- src/LearnCollection/ListExamplePractice1.java
+++ src/LearnCollection/ListExamplePractice1.java
@@ -1,11 +1,18 @@
 package LearnCollection;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class ListExamplePractice1 {
 	
 	public static void main(String[] args) {
-		
+		LinkedList<Integer> list=new LinkedList<Integer>();
+		list.add(56);
+		list.add(60);
+		list.add(45);
+		list.add(null);
+        System.out.println(list);
+		System.out.println("===================");
 		
 		
 		
