c6cbe7ce4c471e12e8fb13a31b2f204fecee34b2
unknown <chogalesanika8@gmail.com>
2024-11-27 13:03:40 +0530

    initial

--- src/LearnCollection/ListExamplePractice1.java
+++ src/LearnCollection/ListExamplePractice1.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.function.Predicate;
@@ -16,6 +17,9 @@ public class ListExamplePractice1 {
 		list.add(89);
 		list.add(35);
 		list.add(12);
+		System.out.println(list);
+		
+		list.iterator();
 		System.out.println(list);
 		
 	}
