88c45609d985c50cf985a8dbb8c3410565d67ad4
unknown <chogalesanika8@gmail.com>
2024-11-27 12:07:53 +0530

    initial

--- src/LearnCollection/ListExamplePractice1.java
+++ src/LearnCollection/ListExamplePractice1.java
@@ -7,7 +7,7 @@ public class ListExamplePractice1 {
 	public static void main(String[] args) {
 		ArrayList<String> list = new ArrayList<String>();
 		list.add("apple");
-		
+		list.add("banana");
 		
 	}
 
