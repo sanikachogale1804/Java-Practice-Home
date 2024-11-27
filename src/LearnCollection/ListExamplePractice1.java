e92ce721ffdfebe18d3e815d0edf1a9051381f70
unknown <chogalesanika8@gmail.com>
2024-11-27 12:17:40 +0530

    initial

--- src/LearnCollection/ListExamplePractice1.java
+++ src/LearnCollection/ListExamplePractice1.java
@@ -5,10 +5,14 @@ import java.util.ArrayList;
 public class ListExamplePractice1 {
 	
 	public static void main(String[] args) {
-		ArrayList<String> list = new ArrayList<String>();
-		list.add("apple");
-		list.add("banana");
-		list.add("kivi");
+		ArrayList<Integer> list=new ArrayList<Integer>();
+		list.add(78);//0 index
+		list.add(78);//1 index
+		list.add(89);//2 index
+		list.add(56);//3 index
+		list.add(32);
+		System.out.println(list);
+		
 	}
 
 }
