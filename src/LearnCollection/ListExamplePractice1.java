8852e5b8c50c30694e4ab6f894a5133da055cf12
unknown <chogalesanika8@gmail.com>
2024-11-27 12:44:36 +0530

    initial

--- src/LearnCollection/ListExamplePractice1.java
+++ src/LearnCollection/ListExamplePractice1.java
@@ -7,27 +7,13 @@ import java.util.function.Predicate;
 public class ListExamplePractice1 {
 	
 	public static void main(String[] args) {
-		LinkedList<Integer> list=new LinkedList<Integer>();
-		list.add(56);
-		list.add(60);
-		list.add(45);
-        System.out.println(list);
-		System.out.println("===================");
+		LinkedList names=new LinkedList<String>();
+		names.add("sanika");
+		names.add("riya");
+		names.add("akshata");
+		names.add("tushar");
+		System.out.println(names);
 		
-//		list.add(0,34);
-//		System.out.println(list);
-//		
-//		System.out.println(list.get(2));
-//		System.out.println("===================");
-//		Integer i = list.set(3, 30);
-//		System.out.println(i);
-//		System.out.println(list);
-		
-//		Predicate<Integer> even=(num)->num%2==0;
-//		list.removeIf(even);
-		
-		list.removeIf((num)->num%2==0);
-		System.out.println(list);
 		
 	}
 
