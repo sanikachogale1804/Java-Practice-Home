5a470f8f90c3af560fdf87a7d609fbcef7da3e46
unknown <chogalesanika8@gmail.com>
2024-11-28 20:06:10 +0530

    initial

--- src/LearnCollection/MapExamplePractice2.java
+++ src/LearnCollection/MapExamplePractice2.java
@@ -1,8 +1,16 @@
 package LearnCollection;
 
+import java.util.LinkedHashMap;
+
 public class MapExamplePractice2 {
 	
 	public static void main(String[] args) {
+		
+		LinkedHashMap<String,String> linkedHashMap = new LinkedHashMap<String, String>();
+		linkedHashMap.put("sanika", "762167878");
+		linkedHashMap.put("riya","8765490734");
+	  	linkedHashMap.put("tushar","7864390117");
+		System.out.println(linkedHashMap);
 		
 	}
 
