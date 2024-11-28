544314c5f6d59084901b5269e073e0fec144f054
unknown <chogalesanika8@gmail.com>
2024-11-28 19:53:11 +0530

    initial

--- src/LearnCollection/MapExamplePractice1.java
+++ src/LearnCollection/MapExamplePractice1.java
@@ -6,9 +6,11 @@ public class MapExamplePractice1 {
 
 	public static void main(String[] args) {
 		
-		HashMap<Integer, String> hashMap = new HashMap<Integer, String>();
-		
-		
+		HashMap<Integer, String> students = new HashMap<Integer, String>();
+		students.put(101, "Sanika");
+		students.put(102, "sneha");
+		students.put(102, "sneha");
+		students.put(103, "akshata");
 		
 	}
 	
