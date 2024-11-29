1c23808e20b140f8b0e99c05f9afe648732b68cb
unknown <chogalesanika8@gmail.com>
2024-11-29 13:15:16 +0530

    initial

--- src/Project1/Book.java
+++ src/Project1/Book.java
@@ -1,46 +1,10 @@
 package Project1;
 
 public class Book {
+	
+	private String title;
+    private String author;
+    private int bookNumber;  // Unique identifier for the book
+    private boolean isAvailable;  // To track if the book is available or not
 
-	 private String title;
-	 private String author;
-	 private int bookNumber;
-	 
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getAuthor() {
-		return author;
-	}
-	public void setAuthor(String author) {
-		this.author = author;
-	}
-	public int getBookNumber() {
-		return bookNumber;
-	}
-	public void setBookNumber(int bookNumber) {
-		bookNumber = bookNumber;
-	}
-	
-	Book()
-	{
-		
-	}
-	
-	Book(String title,String author,int bookNumber)
-	{
-		this.title=title;
-		this.author=author;
-		this.bookNumber=bookNumber;
-	}
-	
-	@Override
-	public String toString() {
-		return "Book [title=" + title + ", author=" + author + ", bookNumber=" + bookNumber + "]";
-	}
-	
-	
 }
