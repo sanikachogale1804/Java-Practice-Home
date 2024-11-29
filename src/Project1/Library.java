1efd46e4a8d77de9ba114ab18794cf62e9bf051f
unknown <chogalesanika8@gmail.com>
2024-11-29 12:44:21 +0530

    initial

--- src/Project1/Library.java
+++ src/Project1/Library.java
@@ -1,7 +1,47 @@
 package Project1;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class Library {
 	
-	
+	  List<Book> books;
+
+	    public Library() {
+	        this.books = new ArrayList<>();
+	    }
+
+	    // Method to add a book to the library
+	    public void addBook(Book book) {
+	        books.add(book);
+	        System.out.println("Book added: " + book);
+	    }
+
+	    // Method to remove a book by ISBN
+	    public void removeBook(int bookNumber) {
+	    	Iterator<Book> iterator = books.iterator();
+	        while (iterator.hasNext()) {
+	            Book book = iterator.next();
+	            if (book.getBookNumber() == bookNumber) {
+	                iterator.remove();
+	                System.out.println("Book removed: " + book);
+	                return;
+	            }
+	        }
+	        System.out.println("Book with ISBN " + bookNumber + " not found.");
+	    }
+
+	    // Method to display all books
+	    public void displayBooks() {
+	        if (books.isEmpty()) {
+	            System.out.println("No books available in the library.");
+	        } else {
+	            System.out.println("Books in the library:");
+	            for (Book book : books) {
+	                System.out.println(book);
+	            }
+	        }
+	    }
 
 }
---
+++ src/Project1/LibraryManagementSystem.java
@@ -0,0 +1,9 @@
+package Project1;
+
+public class LibraryManagementSystem {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
