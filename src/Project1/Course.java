e32caab18e523ed41e3a491315585d78a700eb63
unknown <chogalesanika8@gmail.com>
2024-11-29 14:39:28 +0530

    initial

---
+++ src/Project1/Course.java
@@ -0,0 +1,32 @@
+package Project1;
+
+public class Course {
+	
+	    private String courseName;
+	    private String courseCode;
+
+	    // Constructor
+	    public Course(String courseName, String courseCode) {
+	        this.courseName = courseName;
+	        this.courseCode = courseCode;
+	    }
+
+	    // Get course name
+	    public String getCourseName() {
+	        return courseName;
+	    }
+
+	    // Get course code
+	    public String getCourseCode() {
+	        return courseCode;
+	    }
+
+	    // View course information
+	    public void viewCourseInfo() {
+	        System.out.println("Course Name: " + courseName);
+	        System.out.println("Course Code: " + courseCode);
+	    }
+	}
+
+
+}
