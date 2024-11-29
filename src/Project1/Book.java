674e9408b96b0a8f40b2e2211c1febd3ed6dc9bf
unknown <chogalesanika8@gmail.com>
2024-11-29 12:30:55 +0530

    initial

--- src/Project1/Book.java
+++ src/Project1/Book.java
@@ -5,6 +5,30 @@ public class Book {
 	 private String title;
 	 private String author;
 	 private String BookNumber;
+	 
+//	public String getTitle() {
+//		return title;
+//	}
+//	public void setTitle(String title) {
+//		this.title = title;
+//	}
+//	public String getAuthor() {
+//		return author;
+//	}
+//	public void setAuthor(String author) {
+//		this.author = author;
+//	}
+//	public String getBookNumber() {
+//		return BookNumber;
+//	}
+//	public void setBookNumber(String bookNumber) {
+//		BookNumber = bookNumber;
+//	}
 	
+	public String getTitle()
+	{
+		return title;
+	}
+
 	
 }
