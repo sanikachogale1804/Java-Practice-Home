c1c133786e6d054f51066cd320d431c8f1fe0e6d
unknown <chogalesanika8@gmail.com>
2024-11-29 12:35:13 +0530

    initial

--- src/Project1/Book.java
+++ src/Project1/Book.java
@@ -37,5 +37,10 @@ public class Book {
 		this.bookNumber=bookNumber;
 	}
 	
+	@Override
+	public String toString() {
+		return "Book [title=" + title + ", author=" + author + ", bookNumber=" + bookNumber + "]";
+	}
+	
 	
 }
