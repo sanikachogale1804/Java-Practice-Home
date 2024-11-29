ab2ea28db948faa892bf6341db1ade1857426db6
unknown <chogalesanika8@gmail.com>
2024-11-29 13:19:51 +0530

    initial

--- src/Project1/Book.java
+++ src/Project1/Book.java
@@ -4,7 +4,39 @@ public class Book {
 	
 	private String title;
     private String author;
-    private int bookNumber; 
+    private int bookNumber;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public int getBookNumber() {
+		return bookNumber;
+	}
+	public void setBookNumber(int bookNumber) {
+		this.bookNumber = bookNumber;
+	}
+	@Override
+	public String toString() {
+		return "Book [title=" + title + ", author=" + author + ", bookNumber=" + bookNumber + "]";
+	}
+	public Book(String title, String author, int bookNumber) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.bookNumber = bookNumber;
+	}
+	public Book() {
+		super();
+		// TODO Auto-generated constructor stub
+	} 
    
     
 }
