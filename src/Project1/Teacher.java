7c3a1e4839391fa24d4310264f2b96ccc2dd34c1
unknown <chogalesanika8@gmail.com>
2024-11-29 14:38:47 +0530

    initial

---
+++ src/Project1/Teacher.java
@@ -0,0 +1,46 @@
+package Project1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Teacher {
+
+	    private String name;
+	    private String teacherId;
+	    private List<String> courses;
+
+	    // Constructor
+	    public Teacher(String name, String teacherId) {
+	        this.name = name;
+	        this.teacherId = teacherId;
+	        this.courses = new ArrayList<>();
+	    }
+
+	    // Get teacher name
+	    public String getName() {
+	        return name;
+	    }
+
+	    // Get teacher ID
+	    public String getTeacherId() {
+	        return teacherId;
+	    }
+
+	    // Assign a course to the teacher
+	    public void assignCourse(String course) {
+	        courses.add(course);
+	    }
+
+	    // View teacher information
+	    public void viewTeacherInfo() {
+	        System.out.println("Teacher Name: " + name);
+	        System.out.println("Teacher ID: " + teacherId);
+	        System.out.println("Courses Assigned: ");
+	        for (String course : courses) {
+	            System.out.println(" - " + course);
+	        }
+	    }
+	}
+
+
+}
