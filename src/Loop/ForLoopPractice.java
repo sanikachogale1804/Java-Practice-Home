5081eeb60964699fa953bfeebe81c1c22244050c
unknown <chogalesanika8@gmail.com>
2024-11-30 21:48:31 +0530

    initial

--- src/Loop/ForLoopPractice.java
+++ src/Loop/ForLoopPractice.java
@@ -7,7 +7,7 @@ public class ForLoopPractice {
 	public static void main(String[] args) {
 		
 		Scanner sc=new Scanner(System.in);
-		
+		System.out.println("Enter the number of limits:");
 		
 	}
 
