f381e9a627b4913faa39ae538294cbe970172623
unknown <chogalesanika8@gmail.com>
2024-12-03 19:27:01 +0530

    initial

--- src/StreamPractice/Example9.java
+++ src/StreamPractice/Example9.java
@@ -11,7 +11,7 @@ public class Example9 {
 	public static void main(String[] args) {
 	
 		String words[]= {"apple","banana","kivi"};
-		Arrays.stream(words).forEach(word->word.toLowerCase());
+		Arrays.stream(words).forEach(word->System.out.println(word.toLowerCase()));
 		
 	}
 
