4cb4386fa5cc98ef547742c531473b77619d6651
unknown <chogalesanika8@gmail.com>
2024-12-03 19:17:45 +0530

    initial

--- src/StreamPractice/Example9.java
+++ src/StreamPractice/Example9.java
@@ -2,6 +2,7 @@ package StreamPractice;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -10,7 +11,8 @@ public class Example9 {
 	public static void main(String[] args) {
 	
 		List<Integer> numbers = Arrays.asList(12,78,45,67,34,23);
-		numbers.stream().forEach(()->);
+		
+		numbers.stream().forEach((i)->System.out.println(i+i));
 	}
 
 }
