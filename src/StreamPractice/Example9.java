9bbb00f3f0f372da824e3edc478f9c89c6f90545
unknown <chogalesanika8@gmail.com>
2024-12-03 19:14:00 +0530

    initial

--- src/StreamPractice/Example9.java
+++ src/StreamPractice/Example9.java
@@ -10,7 +10,7 @@ public class Example9 {
 	public static void main(String[] args) {
 	
 		int[] numbers= {12,56,24,67,89,35};
-		Arrays.stream(numbers).forEach();
+		Arrays.stream(numbers).forEach(()->);
 		
 	}
 
