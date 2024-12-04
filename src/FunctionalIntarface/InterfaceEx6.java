7d5b1ba3e5f4df8e0241d9fde72c43657b683baf
unknown <chogalesanika8@gmail.com>
2024-12-04 19:47:51 +0530

    initial

--- src/FunctionalIntarface/InterfaceEx6.java
+++ src/FunctionalIntarface/InterfaceEx6.java
@@ -4,7 +4,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 
-public interface Even
+@FunctionalInterface
+public interface isEven
 {
 	boolean test(int num);
 }
