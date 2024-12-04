aed7e5cf60c9265a28b2b5d5eca2a6774a1278c5
unknown <chogalesanika8@gmail.com>
2024-12-04 19:10:53 +0530

    initial

--- src/FunctionalIntarface/InterfaceEx5.java
+++ src/FunctionalIntarface/InterfaceEx5.java
@@ -1,10 +1,13 @@
 package FunctionalIntarface;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class InterfaceEx5 {
 
 	public static void main(String[] args) {
 		
-		
+		List<String> list = Arrays.asList("apple", "banana", "cherry");
 		
 	}
 	
