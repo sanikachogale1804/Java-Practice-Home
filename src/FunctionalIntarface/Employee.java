e68a330ccf7b9fd10a582a924a826d0ec4f1f993
unknown <chogalesanika8@gmail.com>
2024-12-04 20:07:35 +0530

    initial

--- src/FunctionalIntarface/Employee.java
+++ src/FunctionalIntarface/Employee.java
@@ -6,5 +6,29 @@ public class Employee {
 	private String name;
 	private String department;
 	private int salary;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
 
 }
