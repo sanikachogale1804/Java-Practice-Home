93c24c1ce71c059c5fb338564edcc37d0ebe2401
unknown <chogalesanika8@gmail.com>
2024-12-04 19:19:45 +0530

    initial

--- src/FunctionalIntarface/InterfaceEx6.java
+++ src/FunctionalIntarface/InterfaceEx6.java
@@ -1,8 +1,12 @@
 package FunctionalIntarface;
 
+import java.util.Arrays;
+
 public class InterfaceEx6 {
 	
 	public static void main(String[] args) {
+		
+		Arrays.asList("sanika","sneha","riya");
 		
 	}
 
