579e889034605c75cc3440ef1c3fc0781b178169
unknown <chogalesanika8@gmail.com>
2024-12-04 19:51:07 +0530

    initial

--- src/FunctionalIntarface/InterfaceEx6.java
+++ src/FunctionalIntarface/InterfaceEx6.java
@@ -11,7 +11,7 @@ public class InterfaceEx6 {
 	public static void main(String[] args) {
 		
 		Predicate<Integer> isEven=(num)-> num % 2 == 0;
-		System.out.println(isEven);
+		System.out.println(isEven.test(30));
 		
 	}
 
