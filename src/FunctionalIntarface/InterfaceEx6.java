67498edc1a618da3771dd02f9008598ff464ae16
unknown <chogalesanika8@gmail.com>
2024-12-04 19:25:36 +0530

    initial

--- src/FunctionalIntarface/InterfaceEx6.java
+++ src/FunctionalIntarface/InterfaceEx6.java
@@ -3,6 +3,12 @@ package FunctionalIntarface;
 import java.util.Arrays;
 import java.util.List;
 
+@FunctionalInterface
+public interface Predicate<T>
+{
+	boolean test(T t);
+}
+
 public class InterfaceEx6 {
 	
 	public static void main(String[] args) {
