175e79fa6db4f57d0fff16a60ddf1f3bd220ec23
unknown <chogalesanika8@gmail.com>
2024-12-04 19:44:08 +0530

    initial

--- src/FunctionalIntarface/InterfaceEx6.java
+++ src/FunctionalIntarface/InterfaceEx6.java
@@ -2,13 +2,14 @@ package FunctionalIntarface;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 
 
 public class InterfaceEx6 {
 	
 	public static void main(String[] args) {
 		
-		Predicate<Integer> isEven=(num)->;
+		Predicate<Integer> isEven=(num)-> num % 2 == 0;
 		
 	}
 
