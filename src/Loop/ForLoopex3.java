497b07b7b0d31ef814efae2bb0b80d784a49445a
unknown <chogalesanika8@gmail.com>
2024-12-05 21:57:44 +0530

    initial

--- src/Loop/ForLoopex3.java
+++ src/Loop/ForLoopex3.java
@@ -7,7 +7,8 @@ public class ForLoopex3 {
 	public static void main(String[] args) {
 	
 		Scanner sc=new Scanner(System.in);
-		System.out.println();
+		System.out.println("Enter the digits:");
+		
 		
 	}
 	
