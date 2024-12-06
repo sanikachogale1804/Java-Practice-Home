e587d8d687c34f16a5bcaeae56d47149199b4edb
unknown <chogalesanika8@gmail.com>
2024-12-06 19:01:48 +0530

    initial

--- src/ExceptionHandling/Example3.java
+++ src/ExceptionHandling/Example3.java
@@ -10,7 +10,11 @@ public class Example3 {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello");
-		
+		try {
+			throwException();
+		}catch (Exception e) {
+			// TODO: handle exception
+		}
 		
 	}
 
