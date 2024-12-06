9943b07c16d1236dff881aeb7311e5b5008fac7a
unknown <chogalesanika8@gmail.com>
2024-12-06 19:09:33 +0530

    initial

--- src/ExceptionHandling/Example3.java
+++ src/ExceptionHandling/Example3.java
@@ -7,13 +7,18 @@ public class Example3 {
 		throw new NullPointerException("sanika");
 	}
 	
+	public static void throwExceprion1()
+	{
+		
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Hello");
 		try {
 			throwException();
 		}catch (NullPointerException e) {
-		System.out.println("Exception handled");
+		System.out.println(e);
 		}
 		
 	}
