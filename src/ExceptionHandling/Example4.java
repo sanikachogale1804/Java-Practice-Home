929faefb74b8e4c328ae3b7e0602443db7730539
unknown <chogalesanika8@gmail.com>
2024-12-08 21:49:47 +0530

    initial

--- src/ExceptionHandling/Example4.java
+++ src/ExceptionHandling/Example4.java
@@ -1,16 +1,39 @@
 package ExceptionHandling;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Example4 {
 	
-	public static void fileHandline()
+	public static void fileHandline() throws FileNotFoundException
 	{
-		
+		new FileInputStream("abc.txt");
 	}
 	
+	public static void readFile(String fileName) throws IOException
+	{
+		if(fileName==null)
+		{
+			throw new IllegalArgumentException("File cannot be null");
+		}
+	}
 	
 	public static void main(String[] args) {
 		
+		System.out.println("sanika");
+		try {
+			fileHandline();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
+		try {
+			readFile("abs.txt");
+		} catch (IOException e) {
+			System.out.println(e);
+		}
 	}
 
 }
