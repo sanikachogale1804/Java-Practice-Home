4ef8351eca31ac5abde9916be0dfcd6d5832909e
unknown <chogalesanika8@gmail.com>
2024-12-15 20:56:25 +0530

    initial

--- src/Loop/ForLoopex3.java
+++ src/Loop/ForLoopex3.java
@@ -6,19 +6,7 @@ public class ForLoopex3 {
 
 	public static void main(String[] args) {
 	
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter fibonacci series:");
-		int num = sc.nextInt();
-		int a=0;int b=1;int c=0;
 		
-		for(int i=1;i<=num;i++)
-		{
-            System.out.println(c);
-            a=b;
-            b=c;
-            c=a+b;
-            
-		}
 		
 	}
 	
