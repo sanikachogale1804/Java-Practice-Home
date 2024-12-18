bd6de539d44482376d5904b37c782ba87af4a21c
unknown <chogalesanika8@gmail.com>
2024-12-18 20:46:38 +0530

    initial

--- src/Loop/ForLoopEx4.java
+++ src/Loop/ForLoopEx4.java
@@ -7,15 +7,15 @@ public class ForLoopEx4 {
 	public static void main(String[] args) {
 	
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter fibonacci series:");
+		System.out.println("Enter numbers");
 		int n = sc.nextInt();
-		int a=0,b=1,c=0;
-		for(int i=1;i<=n;i++)
+		int count=0;
+		for(int i=2;i<=num;i++)
 		{
-			System.out.println(c);
-			a=b;
-			b=c;
-			c=a+b;
+			if(i%2==0)
+			{
+				
+			}
 		}
 		
 	}
