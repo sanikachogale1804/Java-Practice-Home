37edfc5af7d88ca793fdb508b0af7516a9dfc185
unknown <chogalesanika8@gmail.com>
2024-12-21 12:17:31 +0530

    initial

--- src/Loop/PracticeExample1.java
+++ src/Loop/PracticeExample1.java
@@ -6,6 +6,8 @@ public class PracticeExample1 {
 	public static void main(String[] args) {
 		
 		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Principal amount: ");
+		System.out.println("Enter rate of Interest");
 		
 		
 	}
