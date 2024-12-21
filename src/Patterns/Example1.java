828739778a51944eebf8f40e312551e270a6c7c5
unknown <chogalesanika8@gmail.com>
2024-12-21 19:20:45 +0530

    initial

--- src/Patterns/Example1.java
+++ src/Patterns/Example1.java
@@ -1,5 +1,15 @@
 package Patterns;
 
 public class Example1 {
+	
+	public static void main(String[] args) {
+		
+		int rows=5;
+		for(int i=1;i<=rows;i++)
+		{
+			System.out.println("*");
+		}
+		
+	}
 
 }
