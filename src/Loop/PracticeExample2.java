c07eca5c0c8c101d4db916b086fff7f6fd89c503
unknown <chogalesanika8@gmail.com>
2024-12-21 13:12:59 +0530

    initial

--- src/Loop/PracticeExample2.java
+++ src/Loop/PracticeExample2.java
@@ -9,10 +9,10 @@ public static void main(String[] args) {
 	System.out.println("Enter the basic of salary");
 	double basicSalary = sc.nextDouble();
 	
-	double dietAllowance=basicSalary*0.20;
-	double HouseRentAllowance=basicSalary*0.30;
+	double dietAllowance=0.20*basicSalary;
+	double HouseRentAllowance=0.30*basicSalary;
 	
-	double grossSalary=basicSalary+dietAllowance*HouseRentAllowance;
+	double grossSalary=basicSalary+dietAllowance+HouseRentAllowance;
 	System.out.println(grossSalary);
 	
 }
