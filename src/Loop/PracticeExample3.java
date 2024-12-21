286e7ffff134d4d18d3aefeb6a6f5460d4081de0
unknown <chogalesanika8@gmail.com>
2024-12-21 18:38:19 +0530

    initial

--- src/Loop/PracticeExample3.java
+++ src/Loop/PracticeExample3.java
@@ -6,10 +6,8 @@ public class PracticeExample3 {
 	
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		double celcius = sc.nextDouble();
-		
-		double farhreneit=celcius*9/5+32;
-		System.out.println(farhreneit);
+		System.out.println("Enter the 3 digit number");
+		double num = sc.nextDouble();
 		
 	}
 
