e6680070e349e85acf98c11569c4ad5cf09861c9
unknown <chogalesanika8@gmail.com>
2024-12-21 19:25:00 +0530

    initial

--- src/Patterns/Example1.java
+++ src/Patterns/Example1.java
@@ -5,14 +5,15 @@ public class Example1 {
 	public static void main(String[] args) {
 		
 		int rows=5;
-		for(int i=1;i<=rows;i++)
+		for(int i=1;i<rows;i++)
 		{
 			for(int j=1;j<=i;j++)
 			{
-				System.out.println( );
 				System.out.println("*");
 				
 			}
+
+			System.out.println();
 			
 		}
 		
