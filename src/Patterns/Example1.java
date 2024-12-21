0d0dd51d337e333b05a0955c25995a8cf9c9f165
unknown <chogalesanika8@gmail.com>
2024-12-21 19:27:29 +0530

    initial

--- src/Patterns/Example1.java
+++ src/Patterns/Example1.java
@@ -5,16 +5,25 @@ public class Example1 {
 	public static void main(String[] args) {
 		
 		int rows=5;
-		for(int i=1;i<rows;i++)
+		for(int i=1;i<=rows;i++)
 		{
 			for(int j=1;j<=i;j++)
 			{
 				System.out.println("*");
 				
 			}
-
 			System.out.println();
-			
+		}
+		
+		int n=4;
+		System.out.println("1");
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
 		}
 		
 	}
