ee73bee8c15f39dda24fce58b645f80c7adf263d
unknown <chogalesanika8@gmail.com>
2024-12-21 19:28:51 +0530

    initial

--- src/Patterns/Example1.java
+++ src/Patterns/Example1.java
@@ -9,22 +9,13 @@ public class Example1 {
 		{
 			for(int j=1;j<=i;j++)
 			{
-				System.out.println("*");
+				System.out.print("*");
 				
 			}
 			System.out.println();
 		}
 		
-		int n=4;
-		System.out.println("1");
-		for(int i=1;i<=n;i++)
-		{
-			for(int j=1;j<=i;j++)
-			{
-				System.out.print("*");
-			}
-			System.out.println();
-		}
+		
 		
 	}
 
