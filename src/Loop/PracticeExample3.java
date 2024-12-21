b7412fd58cc386543c399c879a0bdb23f9317a88
unknown <chogalesanika8@gmail.com>
2024-12-21 18:15:01 +0530

    initial

--- src/Loop/PracticeExample3.java
+++ src/Loop/PracticeExample3.java
@@ -9,7 +9,13 @@ public class PracticeExample3 {
 		double r = sc.nextDouble();
 		double AreaOfRectangle=3.14*r*r;
 		
+		System.out.println("Area of Rectangle"+AreaOfRectangle);
 		
+	
+		double p = sc.nextDouble();
+		double perimiterOfCircle=2*3.14*r;
+		
+		System.out.println("Perimiter of circle"+perimiterOfCircle);
 	}
 
 }
