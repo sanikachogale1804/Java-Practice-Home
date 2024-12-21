7383940177159d910596f60abccae46ebdd1c73e
unknown <chogalesanika8@gmail.com>
2024-12-21 18:44:33 +0530

    initial

--- src/Loop/PracticeExample3.java
+++ src/Loop/PracticeExample3.java
@@ -7,7 +7,17 @@ public class PracticeExample3 {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter the 3 digit number");
-		double num = sc.nextDouble();
+		int num = sc.nextInt();
+	
+		if(num>=100 && num<=1000)
+		{
+			int hundred=num/100;
+			int tens=(num/10)%10;
+			int ones=num%10;
+			
+			int sum=hundred+tens+ones;
+			System.out.println("sum is"+sum);
+		}
 		
 	}
 
