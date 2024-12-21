d9f1b97e7f4aca999ea7de6da67e8458561319c5
unknown <chogalesanika8@gmail.com>
2024-12-21 18:08:11 +0530

    initial

--- src/Loop/PracticeExample3.java
+++ src/Loop/PracticeExample3.java
@@ -1,8 +1,14 @@
 package Loop;
 
+import java.util.Scanner;
+
 public class PracticeExample3 {
 	
 	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		double r = sc.nextDouble();
+		double AreaOfRectangle=3.14*r*r;
+		
 		
 	}
 
