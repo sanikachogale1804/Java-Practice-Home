dcba307a9e1a936c09926700e4e8993cf18f5a14
unknown <chogalesanika8@gmail.com>
2024-12-21 13:08:58 +0530

    initial

--- src/Loop/PracticeExample2.java
+++ src/Loop/PracticeExample2.java
@@ -6,7 +6,14 @@ public class PracticeExample2 {
 public static void main(String[] args) {
 	
 	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter the basic of salary");
+	double basicSalary = sc.nextDouble();
 	
+	double dietAllowance=basicSalary*0.20;
+	double HouseRentAllowance=basicSalary*0.30;
+	
+	double grossSalary=basicSalary+dietAllowance*HouseRentAllowance;
+	System.out.println(grossSalary);
 	
 }
 
