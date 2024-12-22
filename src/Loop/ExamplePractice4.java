748b119809482a6eb4295788ecb324574d4ad7e2
unknown <chogalesanika8@gmail.com>
2024-12-22 19:38:10 +0530

    initial

--- src/Loop/ExamplePractice4.java
+++ src/Loop/ExamplePractice4.java
@@ -1,10 +1,13 @@
 package Loop;
 
+import java.util.Scanner;
+
 public class ExamplePractice4 {
 
 	//armstrong or not
 	public static void main(String[] args) {
-		
+	
+		Scanner sc=new Scanner(System.in);
 	}
 	
 }
