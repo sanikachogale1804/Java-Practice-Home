9609b246940efb40cc690e098a6f80781abca9cb
unknown <chogalesanika8@gmail.com>
2024-12-22 20:49:01 +0530

    initial

--- src/Loop/ExamplePractice4.java
+++ src/Loop/ExamplePractice4.java
@@ -9,33 +9,7 @@ public class ExamplePractice4 {
 	
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter three digit number:");
-		int num = sc.nextInt();
 		
-		if(num<100 || num>999)
-		{
-			System.out.println("Enter a valid three digit number");
-		}
-		else
-		{
-			int originalNumber=num;
-			int sum=0;
-			
-			while(num>0)
-			{
-				int digit=num%10;
-				sum+=Math.pow(digit,3);
-				num=num/10;
-			}
-			
-			if(sum==originalNumber)
-			{
-				System.out.println("Armstrong number");
-			}
-			else
-			{
-			   System.out.println("Not armstrong number");
-			}
-		}	
 	}
 	
 }
