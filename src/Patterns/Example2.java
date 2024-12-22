f07c9d774e8da4a05897c6d0c64b2ea3761a62d8
unknown <chogalesanika8@gmail.com>
2024-12-22 22:24:11 +0530

    initial

--- src/Patterns/Example2.java
+++ src/Patterns/Example2.java
@@ -9,6 +9,10 @@ public class Example2 {
 		int rows=5;
 		for(int i=1;i<rows;i++)
 		{
+			for(int j=2*(rows-i);j>=0;j++)
+			{
+				System.out.print(" ");
+			}
 			for(int j=1;j<=i;j++)
 			{
 				System.out.print("*");
