fe4e8a43afceb17edbac8a8b2f0caed55c377192
unknown <chogalesanika8@gmail.com>
2024-12-23 21:30:28 +0530

    initial

--- src/Patterns/Example2.java
+++ src/Patterns/Example2.java
@@ -2,11 +2,16 @@ package Patterns;
 
 import java.util.Scanner;
 
+//left half pyramid pattern
 public class Example2 {
 	
 	public static void main(String[] args) {
 		
-		
+		int rows=6;
+		for()
+		{
+			
+		}
 		
 	}
 
