c17d74d81830eb7617b7a9e38881219250bc4572
unknown <chogalesanika8@gmail.com>
2024-12-23 22:08:38 +0530

    initial

--- src/Patterns/Example3.java
+++ src/Patterns/Example3.java
@@ -8,6 +8,14 @@ public class Example3 {
 		Scanner sc=new Scanner(System.in);
 		int n = sc.nextInt();
 		
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(j);
+			}
+			System.out.println();
+		}
 		}   
 
 }
