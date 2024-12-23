07b92df959f7ec0ecbe2a8c7ada6aa8b8fb85264
unknown <chogalesanika8@gmail.com>
2024-12-23 21:28:09 +0530

    initial

--- src/Patterns/Example1.java
+++ src/Patterns/Example1.java
@@ -4,16 +4,7 @@ public class Example1 {
 	
 	public static void main(String[] args) {
 		
-		int rows=5;
-		for(int i=1;i<=rows;i++)
-		{
-			for(int j=1;j<=i;j++)
-			{
-				System.out.print("*");
-				
-			}
-			System.out.println();
-		}	
+		
 	}
 
 }
--- src/Patterns/Example2.java
+++ src/Patterns/Example2.java
@@ -7,9 +7,9 @@ public class Example2 {
 	public static void main(String[] args) {
 		
 		int rows=5;
-		for(int i=1;i<rows;i++)
+		for(int i=1;i<=rows;i++)
 		{
-			for(int j=2*(rows-i);j>=0;j++)
+			for(int j=2*(rows-i);j>=0;j--)
 			{
 				System.out.print(" ");
 			}
