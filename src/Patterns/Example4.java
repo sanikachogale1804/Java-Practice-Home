fb75e01a086f0c6af6d00599eabf3d9098d50f9c
unknown <chogalesanika8@gmail.com>
2024-12-24 22:10:32 +0530

    initial

--- src/Patterns/Example4.java
+++ src/Patterns/Example4.java
@@ -3,6 +3,21 @@ package Patterns;
 public class Example4 {
 	public static void main(String[] args) {
 		
+		int n=5;
+		int i;
+		int j;
+		int num=1;
+		for(i=1;i<=n;i++)
+		{
+			for(j=1;j<=i;j++)
+			{
+				System.out.print(num+" ");
+				num++;
+			}
+		
+			System.out.println();
+		}
+		
 	}
 
 }
