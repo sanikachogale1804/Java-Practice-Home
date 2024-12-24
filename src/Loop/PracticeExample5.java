b12ca628454389228bc1747ceb6740d116bacd4f
unknown <chogalesanika8@gmail.com>
2024-12-24 13:56:52 +0530

    initial

--- src/Loop/PracticeExample5.java
+++ src/Loop/PracticeExample5.java
@@ -2,6 +2,7 @@ package Loop;
 
 import java.util.Scanner;
 
+//perfect or not
 public class PracticeExample5 {
 
 	public static void main(String[] args) {
@@ -13,7 +14,22 @@ public class PracticeExample5 {
 			System.out.println("Not perfectt");
 		}
 		
+		int sum=0;
+		for(int i=1;i<=n/2;i++)
+		{
+			if(n%i==0)
+			{
+				sum+=i;
+			}
+		}
 		
+		if(sum==n) {
+			System.out.println("Perfect number");
+		}
+		else
+		{
+			System.out.println("Not perfect number");
+		}
 	}
 	
 }
---
+++ src/Loop/PracticeExample6.java
@@ -0,0 +1,16 @@
+package Loop;
+
+import java.util.Scanner;
+
+public class PracticeExample6 {
+	
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		
+		int a=0;int b=1;int c=0;
+		
+		
+	}
+
+}
