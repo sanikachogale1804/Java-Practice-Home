d18af940e36af81ad78b29b40c1232e29c17270b
unknown <chogalesanika8@gmail.com>
2024-12-24 13:01:43 +0530

    initial

--- src/Loop/PracticeExample5.java
+++ src/Loop/PracticeExample5.java
@@ -1,8 +1,12 @@
 package Loop;
 
+import java.util.Scanner;
+
 public class PracticeExample5 {
 
 	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n = sc.nextInt();
 		
 	}
 	
