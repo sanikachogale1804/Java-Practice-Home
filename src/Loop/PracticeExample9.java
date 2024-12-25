0fa6fe16c4c756244d58e20158193bc944e796a7
unknown <chogalesanika8@gmail.com>
2024-12-25 11:35:49 +0530

    initial

--- src/Loop/PracticeExample9.java
+++ src/Loop/PracticeExample9.java
@@ -7,12 +7,13 @@ public class PracticeExample9 {
 	
 	public static void main(String[] args) {
 		
-		for(int i=1;i<=10;i++)
+		for(int i=1;i<=5;i++)
 		{
-			for(j=1;j<=i;j++)
+			for(int j=1;j<=i;j++)
 			{
-				
+				System.out.print("*");
 			}
+			System.out.println();
 		}
 		
 	}
