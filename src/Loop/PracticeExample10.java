8fc47ff6b68932bbb199f0473a277c9afaccac60
unknown <chogalesanika8@gmail.com>
2024-12-25 11:39:28 +0530

    initial

--- src/Loop/PracticeExample10.java
+++ src/Loop/PracticeExample10.java
@@ -3,13 +3,14 @@ package Loop;
 public class PracticeExample10 {
 	
 	public static void main(String[] args) {
-		int n=10;
-		
+		int n=5;
+		int num=1;
 		for(int i=1;i<=n;i++)
 		{
 			for(int j=1;j<=i;j++)
 			{
-				System.out.print("*");
+				System.out.print(num+" ");
+				num++;
 			}
 			System.out.println();
 		}
