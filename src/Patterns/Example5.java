ddd5ba0230eb146ac0780f56b9c07a8a69f56c74
unknown <chogalesanika8@gmail.com>
2024-12-25 21:16:17 +0530

    initial

--- src/Patterns/Example5.java
+++ src/Patterns/Example5.java
@@ -9,7 +9,11 @@ public class Example5 {
 		{
 		   for(int j=1;j<=i;j++)
 		   {
-			   System.out.print(i);
+			   if((i+j)==0)
+			   {
+				   System.out.print(1);
+			   }
+			   
 		   }
 		   System.out.println();
 		}
