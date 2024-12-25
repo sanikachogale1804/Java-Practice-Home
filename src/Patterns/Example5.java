985d05bc003ff4a1c1329b2463d7515e533d12b4
unknown <chogalesanika8@gmail.com>
2024-12-25 21:15:30 +0530

    initial

--- src/Patterns/Example5.java
+++ src/Patterns/Example5.java
@@ -4,13 +4,14 @@ public class Example5 {
 
 	public static void main(String[] args) {
 		
-		int n=10;
+		int n=5;
 		for(int i=1;i<=n;i++)
 		{
-		   for()
+		   for(int j=1;j<=i;j++)
 		   {
-			   
+			   System.out.print(i);
 		   }
+		   System.out.println();
 		}
 		
 	}
