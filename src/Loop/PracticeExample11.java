6c1ce3c90263befb8c0afed83e616127f95391cf
unknown <chogalesanika8@gmail.com>
2024-12-25 14:53:21 +0530

    initial

--- src/Loop/PracticeExample11.java
+++ src/Loop/PracticeExample11.java
@@ -1,8 +1,21 @@
 package Loop;
 
+import java.util.Scanner;
+
 public class PracticeExample11 {
 	
 	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		int n = sc.nextInt();
+		if(n<=100 && n>=999)
+		{
+			System.out.println("invalid number");
+		}
+		else
+		{
+			
+		}
 		
 		
 		
