bd514f1cbec2cd20d763e615715abc0a6c185c04
unknown <chogalesanika8@gmail.com>
2024-12-25 11:35:03 +0530

    initial

--- src/Loop/PracticeExample9.java
+++ src/Loop/PracticeExample9.java
@@ -1,10 +1,19 @@
 package Loop;
 
+import java.util.Iterator;
+
 public class PracticeExample9 {
 
 	
 	public static void main(String[] args) {
 		
+		for(int i=1;i<=10;i++)
+		{
+			for(j=1;j<=i;j++)
+			{
+				
+			}
+		}
 		
 	}
 }
