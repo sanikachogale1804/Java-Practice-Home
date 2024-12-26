c1f16d1f467162fc6fea8eab33e84ded1dd6054e
unknown <chogalesanika8@gmail.com>
2024-12-26 21:03:36 +0530

    initial

--- src/ArrayPractice/Example1.java
+++ src/ArrayPractice/Example1.java
@@ -4,6 +4,16 @@ public class Example1 {
 	
 	public static void main(String[] args) {
 		
+		int arr[]= {12,-87,35,89,-34,17};
+		for(int n:arr)
+		{
+			if(n>0)
+			{
+				System.out.println(n);
+			}
+			
+		}
+		
 	}
 
 }
