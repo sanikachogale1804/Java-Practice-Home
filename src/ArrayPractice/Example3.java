40eda542bdb98932098246f8678a1e94f27d33e2
unknown <chogalesanika8@gmail.com>
2024-12-26 21:14:23 +0530

    initial

--- src/ArrayPractice/Example3.java
+++ src/ArrayPractice/Example3.java
@@ -4,6 +4,17 @@ public class Example3 {
 	
 	public static void main(String[] args) {
 		
+		int arr[]= {4,7,8,2,4,3,6,4};
+		int count=0;
+		for(int data:arr)
+		{
+			if(data==4)
+			{
+				count++;
+			}
+		}
+		System.out.println(count);
+		
 	}
 
 }
