e5c2e34c2a883e6a57b2424248a832efcb73b0a3
unknown <chogalesanika8@gmail.com>
2024-12-26 21:05:40 +0530

    initial

--- src/ArrayPractice/Example1.java
+++ src/ArrayPractice/Example1.java
@@ -5,14 +5,15 @@ public class Example1 {
 	public static void main(String[] args) {
 		
 		int arr[]= {12,-87,35,89,-34,17};
+		int count=0;
 		for(int n:arr)
 		{
 			if(n>0)
 			{
-				System.out.println(n);
+				count++;
 			}
-			
 		}
+		System.out.println(count);
 		
 	}
 
