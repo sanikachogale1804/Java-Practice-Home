f0575651dda8914a023e22ca013bf771fb62337b
unknown <chogalesanika8@gmail.com>
2024-12-26 21:09:14 +0530

    initial

--- src/ArrayPractice/Example2.java
+++ src/ArrayPractice/Example2.java
@@ -4,6 +4,16 @@ public class Example2 {
 	
 	public static void main(String[] args) {
 		
+		int arr[]= {12,89,56,35,29,34};
+		int sum=0;
+		for(int num:arr)
+		{
+			if(num%2==1)
+			{
+				
+			}
+		}
+		
 	}
 
 }
