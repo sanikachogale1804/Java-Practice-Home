c8b9c78b837551cfbf4ff0b6283977b78f66b97b
unknown <chogalesanika8@gmail.com>
2024-12-27 20:44:52 +0530

    initial

--- src/Practice/LoopPractice1.java
+++ src/Practice/LoopPractice1.java
@@ -9,10 +9,13 @@ public static void main(String[] args) {
 	int n = sc.nextInt();
 	int a=0,b=1,c=0;
 	
-	for(int i=1;i<=n;i++)
+	for(int i=0;i<=n;i++)
 	{
 		c=a+b;
 		System.out.println(c);
+		a=b;
+		b=c;
+		
 		
 	}
 	
