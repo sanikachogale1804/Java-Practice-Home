43b2c6800e536246a33a5ca7afedb7d24add2904
unknown <chogalesanika8@gmail.com>
2024-12-28 12:25:25 +0530

    initial

--- src/StringExample/PracticeExample5.java
+++ src/StringExample/PracticeExample5.java
@@ -6,6 +6,16 @@ public class PracticeExample5 {
 		
 		int arr[]= {12,67,-78,63,-90,-45};
 		
+		int count=0;
+		for(int i=0;i<arr.length;i++)
+		{
+			if(arr[i]>0)
+			{
+				count++;
+			}
+			
+		}
+		System.out.println(count);
 		
 	}
 
