69b22615f8ef3e1b13b8004cb49e49bf5be48652
unknown <chogalesanika8@gmail.com>
2024-12-28 11:34:54 +0530

    initial

--- src/StringExample/PracticeExample3.java
+++ src/StringExample/PracticeExample3.java
@@ -4,15 +4,16 @@ public class PracticeExample3 {
 	
 	public static void main(String[] args) {
 		
-		int arr[]= {2,6,7,8,9,3,6,5,9};
-		int smallest=0;
+		int arr[]= {3,2,6,7,8,9,3,1,6,5,9};
+		int smallest=arr[0];
 		for(int i=0;i<arr.length;i++)
 		{
-			if(arr[i]>smallest)
+			if(arr[i]<smallest)
 			{
-				System.out.println(arr);
+				smallest=arr[i];
 			}
 		}
+		System.out.println(smallest);
 		
 	}
 
