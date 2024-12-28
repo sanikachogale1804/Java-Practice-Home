c80106b136acd6a84701ec776b79e6293be74b52
unknown <chogalesanika8@gmail.com>
2024-12-28 11:22:37 +0530

    initial

--- src/StringExample/PracticeExample3.java
+++ src/StringExample/PracticeExample3.java
@@ -5,7 +5,14 @@ public class PracticeExample3 {
 	public static void main(String[] args) {
 		
 		int arr[]= {2,6,7,8,9,3,6,5,9};
-		
+		int smallest=0;
+		for(int i=0;i<arr.length;i++)
+		{
+			if(arr[i]>smallest)
+			{
+				System.out.println(arr);
+			}
+		}
 		
 	}
 
