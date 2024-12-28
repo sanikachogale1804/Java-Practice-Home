e02fe07d1e9ff81f986eaa85afe78bad9f455b97
unknown <chogalesanika8@gmail.com>
2024-12-28 12:32:57 +0530

    initial

--- src/StringExample/PracticeExample8.java
+++ src/StringExample/PracticeExample8.java
@@ -5,13 +5,15 @@ public class PracticeExample8 {
 	public static void main(String[] args) {
 		
 		int arr[]= {2,4,6,8,4,9,4};
+		int count=0;
 		for(int data:arr)
 		{
 			if(data==4)
 			{
-				
+				count++;
 			}
 		}
+		System.out.println(count);
 		
 	}
 
