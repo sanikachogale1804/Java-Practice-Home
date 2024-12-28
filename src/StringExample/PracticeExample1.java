1e07a45e9991c17732bbefe8c437cafb99c910ba
unknown <chogalesanika8@gmail.com>
2024-12-28 12:33:32 +0530

    initial

--- src/StringExample/PracticeExample1.java
+++ src/StringExample/PracticeExample1.java
@@ -10,18 +10,8 @@ public class PracticeExample1 {
 		int search = sc.nextInt();
 		int arr[]= {2,4,5,7,2,9,4};
 		
-		int index=0;
-		for(int i=0;i<arr.length;i++)
-		{
+		int index=1;
 		
-			if(arr[i]==search)
-			{
-				index=i;
-				break;
-			}
-			
-		}
-		System.out.println(index);
 		
 	}
 
