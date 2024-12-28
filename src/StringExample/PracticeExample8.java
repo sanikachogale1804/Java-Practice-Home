a6b48014ade19db7baaf9536a78e50d9c0e54da9
unknown <chogalesanika8@gmail.com>
2024-12-28 12:32:22 +0530

    initial

--- src/StringExample/PracticeExample8.java
+++ src/StringExample/PracticeExample8.java
@@ -4,7 +4,14 @@ public class PracticeExample8 {
 	
 	public static void main(String[] args) {
 		
-		
+		int arr[]= {2,4,6,8,4,9,4};
+		for(int data:arr)
+		{
+			if(data==4)
+			{
+				
+			}
+		}
 		
 	}
 
