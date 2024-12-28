3ec00518ce6cfaf48adaba1bea5350efe966cf1e
unknown <chogalesanika8@gmail.com>
2024-12-28 11:14:19 +0530

    initial

--- src/StringExample/PracticeExample2.java
+++ src/StringExample/PracticeExample2.java
@@ -4,6 +4,11 @@ public class PracticeExample2 {
 
 	public static void main(String[] args) {
 		
+		String arr[]= {"Mango","Banana","Kivi","Pineapple","Apple"};
+		for(int i=arr.length-1;i>=0;i--)
+		{
+			System.out.println(arr);
+		}
 	}
 	
 }
