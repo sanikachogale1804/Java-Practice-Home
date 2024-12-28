e4d0287f7a2c22984d436fd7ba7e296c64715a98
unknown <chogalesanika8@gmail.com>
2024-12-28 11:37:27 +0530

    initial

--- src/StringExample/practiceExample4.java
+++ src/StringExample/practiceExample4.java
@@ -1,8 +1,17 @@
 package StringExample;
 
+import java.util.HashSet;
+
 public class practiceExample4 {
 	
 	public static void main(String[] args) {
+		
+		int data[]= {2,6,8,5,2,5,6,8,4};
+		HashSet<Integer> uniqueElements=new HashSet<Integer>();
+		for()
+		{
+			
+		}
 		
 	}
 
