5d0d233ee17f950001f9e4313a0edb56bbf6e1d0
unknown <chogalesanika8@gmail.com>
2024-12-28 10:56:21 +0530

    initial

--- src/StringExample/PracticeExample1.java
+++ src/StringExample/PracticeExample1.java
@@ -1,16 +1,23 @@
 package StringExample;
 
+import java.util.Scanner;
+
 public class PracticeExample1 {
 	
 	public static void main(String[] args) {
 		
+		Scanner sc=new Scanner(System.in);
+		int search = sc.nextInt();
 		int arr[]= {2,4,5,7,2,9,4};
 		
 		int index=1;
 		for(int i=0;i<arr.length;i++)
 		{
 		
-			
+			if(arr[i]==search)
+			{
+				
+			}
 			
 		}
 		
