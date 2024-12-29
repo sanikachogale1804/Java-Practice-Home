cdae091af06ac85ec530c65893c8d6603353a97a
unknown <chogalesanika8@gmail.com>
2024-12-29 19:15:35 +0530

    initial

--- src/Practice/LoopPractice2.java
+++ src/Practice/LoopPractice2.java
@@ -8,20 +8,28 @@ public class LoopPractice2 {
 		Scanner sc=new Scanner(System.in);
 		int n = sc.nextInt();
 		
-		if(n<=100 || n>=999)
+		if(n<100 || n>999)
 		{
 			System.out.println("Enter valid three digit number");
 		}
 		else
 		{
-			int originalNumber=0;
+			int originalNumber=n;
 			int sum=0;
 			
 			while(n>0)
 			{
 				int digit=n%10;
 				sum+=Math.pow(digit, 3);
-				n=n/10;
+				n/=10;
+			}
+			if(sum==originalNumber)
+			{
+				System.out.println("Armstrong");
+			}
+			else
+			{
+				System.out.println("Not armstrong");
 			}
 			
 		}
