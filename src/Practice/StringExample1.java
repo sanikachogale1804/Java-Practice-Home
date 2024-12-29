031c870c520d7085d32aa22738088de1d44144c9
unknown <chogalesanika8@gmail.com>
2024-12-29 20:05:57 +0530

    initial

--- src/Practice/StringExample1.java
+++ src/Practice/StringExample1.java
@@ -5,7 +5,13 @@ public class StringExample1 {
 	public static void main(String[] args) {
 		
 		String str= "I love java programming";
-	    System.out.println(str);	
+	    System.out.println(str);
+	    
+	    for(int i=str.length()-1;i>=0;i--)
+	    {
+	    	System.out.println(str);
+	    }
+	    
 	}
 	
 }
