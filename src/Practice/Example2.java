4b0bd373bc9a034fddf56ff8c27e772afc30272e
unknown <chogalesanika8@gmail.com>
2024-12-29 19:08:01 +0530

    initial

--- src/Practice/Example2.java
+++ src/Practice/Example2.java
@@ -1,9 +1,37 @@
 package Practice;
 
+import java.util.Scanner;
+
 public class Example2 {
 	
 	public static void main(String[] args) {
 		
+		Scanner sc=new Scanner(System.in);
+		int n= sc.nextInt();
+		for(int i=2;i<=n;i++)
+		{
+			if(isPrime(i))
+			{
+				System.out.print(i+",");
+			}
+		}
 	}
-
+		public static boolean isPrime(int num)
+		{
+			if(num<=1)
+			{
+				return false;
+			}
+			
+			for(int i=2;i<=Math.sqrt(num);i++)
+			{
+				if(num%i==0)
+				{
+					return false;
+				}
+			}
+			return true;
+	}
 }
+
+
---
+++ src/Practice/LoopPractice2.java
@@ -0,0 +1,8 @@
+package Practice;
+
+public class LoopPractice2 {
+	public static void main(String[] args) {
+		
+	}
+
+}
