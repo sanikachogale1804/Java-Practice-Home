b114b2517d5f5c853d9295a9df5bc406d426e221
unknown <chogalesanika8@gmail.com>
2025-01-06 17:58:24 +0530

    initial

--- src/Practice/FirstProgram.java
+++ src/Practice/FirstProgram.java
@@ -6,11 +6,9 @@ public class FirstProgram {
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		int n = sc.nextInt();
-		if(n<100 && n>999)
-		{
-			
-		}
+		int number = sc.nextInt();
+		
+		
 	}
 
 }
