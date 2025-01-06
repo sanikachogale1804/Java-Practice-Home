f4b7f40405b1c7aa1370151d92e2d811e75b89e7
unknown <chogalesanika8@gmail.com>
2025-01-06 17:57:58 +0530

    initial

--- src/Practice/FirstProgram.java
+++ src/Practice/FirstProgram.java
@@ -1,11 +1,16 @@
 package Practice;
 
+import java.util.Scanner;
+
 public class FirstProgram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		System.out.println("hello");
+		Scanner sc=new Scanner(System.in);
+		int n = sc.nextInt();
+		if(n<100 && n>999)
+		{
+			
+		}
 	}
 
 }
