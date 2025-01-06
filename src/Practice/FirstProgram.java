b42c56d3f8edf6e675cc37be034433a91c78e7ab
unknown <chogalesanika8@gmail.com>
2025-01-06 17:58:46 +0530

    initial

--- src/Practice/FirstProgram.java
+++ src/Practice/FirstProgram.java
@@ -6,6 +6,7 @@ public class FirstProgram {
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number");
 		int number = sc.nextInt();
 		
 		
