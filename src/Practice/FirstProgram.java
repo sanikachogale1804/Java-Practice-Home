8486e72296ace32a182411a1cfffa74d806f6796
unknown <chogalesanika8@gmail.com>
2025-01-16 19:21:38 +0530

    initial

--- src/Practice/FirstProgram.java
+++ src/Practice/FirstProgram.java
@@ -6,8 +6,10 @@ public class FirstProgram {
 
 	public static void main(String[] args) {
 
-		System.out.println(false?"sanika":"sneha");
-		
+		int a=50;
+		int b=30;
+		int biggvalue=(a>b && a<b?a:b);
+		System.out.println(biggvalue);
 	}
 	
 }
