1a59e26f5f1ca2c2f47e12a775d6ce1a220e2a39
unknown <chogalesanika8@gmail.com>
2025-01-16 19:15:58 +0530

    initial

--- src/Practice/FirstProgram.java
+++ src/Practice/FirstProgram.java
@@ -5,30 +5,9 @@ import java.util.Scanner;
 public class FirstProgram {
 
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter a number");
-		int number = sc.nextInt();
-		
-		if(isPalindrome(number))
-		{
-			System.out.println("Palindrome");
-		}
-		else
-		{
-			System.out.println("Not palindrome");
-		}
-		
-	}
-	public static boolean isPalindrome(number)
-	{
-		int originalNumber=number;
-		int reversedNumber=0;
-		
-		while(number!=0)
-		{
-			int lastDigit=number%10;
-			
-		}
-	}
 
+	
+		
+	}
+	
 }
