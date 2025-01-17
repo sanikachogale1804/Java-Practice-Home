f2f6bf0c3dc203f8bcbf5a3a308f1ce052385c7a
unknown <chogalesanika8@gmail.com>
2025-01-17 16:32:04 +0530

    initial

--- src/Practice/FirstProgram.java
+++ src/Practice/FirstProgram.java
@@ -6,18 +6,6 @@ public class FirstProgram {
 
 	public static void main(String[] args) {
 
-		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		System.out.println("1:monday 2:Tuesday 3:wednesday 4:thursday 5:friday 6:saturday 7:sunday");
-
-		switch(n)
-		{
-		case 1:
-			System.out.println("Monday");
-			break;
-		case 2:
-			System.out.println("Tuesday");
-		} 
 		
 	}
 	
