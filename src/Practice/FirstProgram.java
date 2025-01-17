310f9820b703ef435fe42df3a4f00b09c373c0e6
unknown <chogalesanika8@gmail.com>
2025-01-17 16:44:13 +0530

    initial

--- src/Practice/FirstProgram.java
+++ src/Practice/FirstProgram.java
@@ -6,16 +6,15 @@ public class FirstProgram {
 
 	public static void main(String[] args) {
 
-		int numbers[]= {1,2,3,4,5};
-		System.out.println(numbers[0]);
-		System.out.println(numbers.length-1);
-		System.out.println("=====================");
-		String names[]= {"sanika","riya","sneha"};
-		System.out.println(names[0]);
-		int numbers1[][]= {{23,12,56},{54,34,67}};
-		System.out.println(numbers1[1][2]);
-		String names2[][]= {{"sanika","sneha"},{"siddhesh","tushar"}};
-		System.out.println(names2[0][1]);
+		int numbers1[]= {23,12,56};
+		for(int i=0;i<numbers1.length;i++)
+		{
+			System.out.println(numbers1[i]);
+		}
+		for()
+		{
+			
+		}
 	}
 	
 }
